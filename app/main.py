af0f22aabcdd2d495ce99396547a02ebb066d3db
Dima Svirsa <august4smile@gmail.com>
2022-06-10 16:49:21 +0300

    no extra

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+
 class Deck:
     def __init__(self, row, column, is_alive=True):
         pass
@@ -20,15 +21,39 @@ class Ship:
 
 class Battleship:
     def __init__(self, ships):
-        # Create a dict `self.field`.
-        # Its keys are tuples - the coordinates of the non-empty cells,
-        # A value for each cell is a reference to the ship
-        # which is located in it
-        pass
+        self.field = {}
+
+        for ship in ships:
+            len_x, len_y = [abs(x - y) for x, y in zip(ship[0], ship[1])]
+            for step in range(len_x):
+                self.field[(ship[0][0] + step, ship[0][1])] = ship
+            for step in range(len_y):
+                self.field[(ship[0][0], ship[0][1] + step)] = ship
+            self.field[ship[1]] = ship
 
     def fire(self, location: tuple):
-        # This function should check whether the location
-        # is a key in the `self.field`
-        # If it is, then it should check if this cell is the last alive
-        # in the ship or not.
-        pass
+        if location in self.field:
+            the = self.field[location]
+            self.field[location] = f"{self.field[location]} Hit!"
+            if the not in self.field.values():
+                return "Sunk!"
+            return "Hit!"
+        return "Miss!"
+
+
+if __name__ == "__main__":
+    battle_ship = Battleship(
+        ships=[
+                    ((2, 0), (2, 3)),
+                    ((4, 5), (4, 6)),
+                    ((3, 8), (3, 9)),
+                    ((6, 0), (8, 0)),
+                    ((6, 4), (6, 6)),
+                    ((6, 8), (6, 9)),
+                    ((9, 9), (9, 9)),
+                    ((9, 5), (9, 5)),
+                    ((9, 3), (9, 3)),
+                    ((9, 7), (9, 7)),
+                ]
+    )
+    print(battle_ship.field)
