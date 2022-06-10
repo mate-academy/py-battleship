12a53017a3a327425bed6137578c09515e14bb2e
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-06-11 01:11:13 +0300

    Add func: create field, set ships on field, and show field with destroy decks of ships"

--- app/main.py
+++ app/main.py
@@ -5,6 +5,7 @@ from app.ship import Ship
 
 
 class Battleship:
+
     def __init__(self, ships: List[tuple]):
         # Create a dict `self.field`.
         # Its keys are tuples - the coordinates of the non-empty cells,
@@ -12,6 +13,9 @@ class Battleship:
         # which is located in it
         self.ships = ships
         self.fleet = self._create_fleet()
+        self.field_size = (9, 9)
+        self.field = self._create_field()
+        self.icon_deck = u"\u25A1"
 
     def _create_fleet(self):
         return {
@@ -29,11 +33,83 @@ class Battleship:
         # If it is, then it should check if this cell is the last alive
         # in the ship or not.
         for ship in self.fleet.values():
-            deck = ship.get_deck(location[0], location[1])
+            deck = ship.get_deck(*location)
+            location_hit_deck = []
+            if deck and not ship.check_sunk():
 
-            if deck and not ship.chech_sunk():
+                location_hit_deck.append(location)
+                for row in self.field:
+                    if location in row:
+                        row[location] = "*"
+
                 return ship.hit_in_deck(deck)
-            elif deck and ship.chech_sunk():
+
+            elif deck and ship.check_sunk():
+
+                location_hit_deck.append(location)
+                for row in self.field:
+                    if location in row:
+                        row[location] = "*"
+
                 return ship.sunk_ship(deck)
 
         return ship.miss_in_ship()
+
+    def _create_field(self):
+        return [
+            {
+                (hight, width): "~" for width in range(self.field_size[1] + 1)
+            }
+            for hight in range(self.field_size[0] + 1)
+        ]
+
+    def set_ships_on_field(self):
+        list_all_decks_cord = [
+            (deck.row, deck.column)
+            for ship in self.fleet.values()
+            for deck in ship.decks
+        ]
+        for row in self.field:
+            for cord in list_all_decks_cord:
+                if cord in row:
+                    row[cord] = self.icon_deck
+
+    def print_field(self):
+        for line in self.field:
+            print(*list(line.values()), sep="   ")
+
+
+if __name__ == '__main__':
+    battle_ship = Battleship(
+        ships=[
+            ((2, 0), (2, 3)),
+            ((4, 5), (4, 6)),
+            ((3, 8), (3, 9)),
+            ((6, 0), (8, 0)),
+            ((6, 4), (6, 6)),
+            ((6, 8), (6, 9)),
+            ((9, 9), (9, 9)),
+            ((9, 5), (9, 5)),
+            ((9, 3), (9, 3)),
+            ((9, 7), (9, 7)),
+        ]
+    )
+    battle_ship.print_field()
+    battle_ship.set_ships_on_field()
+    print("--" * 100)
+    battle_ship.print_field()
+    print(battle_ship.fire((0, 4)) == "Miss!")
+    print(battle_ship.fire((1, 7)) == "Miss!")
+    print(battle_ship.fire((2, 0)) == "Hit!")
+    print(battle_ship.fire((2, 1)) == "Hit!")
+    print(battle_ship.fire((2, 2)) == "Hit!")
+    print(battle_ship.fire((2, 3)) == "Sunk!")
+    print(battle_ship.fire((4, 3)) == "Miss!")
+    print(battle_ship.fire((4, 5)) == "Hit!")
+    print(battle_ship.fire((5, 5)) == "Miss!")
+    print(battle_ship.fire((4, 6)) == "Sunk!")
+    print(battle_ship.fire((9, 5)) == "Sunk!")
+    print(battle_ship.fire((9, 6)) == "Miss!")
+    print(battle_ship.fire((9, 5)) == "Sunk!")
+    print("--" * 100)
+    battle_ship.print_field()
--- app/ship.py
+++ app/ship.py
@@ -4,23 +4,23 @@ class Ship:
         self.start = start
         self.end = end
         self.is_drowned = is_drowned
-        self.decks = self.all_decks_ship()
+        self.decks = self._all_decks_ship()
 
     def get_deck(self, row, column):
         # Find the corresponding deck in the list
         for deck in self.decks:
-            if isinstance(deck, Deck):
-                if deck.row == row and deck.column == column:
-                    return deck
+            if isinstance(deck, Deck) and \
+                    deck.row == row and deck.column == column:
+                return deck
 
     def _kill_deck(self, deck):
         # Change the `is_alive` status of the deck
         # And update the `is_drowned` value if it's needed
         index_the_same_cord = self.decks.index(deck)
-        self.decks[index_the_same_cord] = "*"
+        self.decks[index_the_same_cord] = None
         deck.is_alive = False
 
-    def all_decks_ship(self):
+    def _all_decks_ship(self):
         x, y = self.start
         x_end, y_end = self.end
         self.decks_ship = [Deck(x, y)]
@@ -45,10 +45,10 @@ class Ship:
         self._kill_deck(deck)
         return "Sunk!"
 
-    def chech_sunk(self):
-        if self.decks.count("*") == len(self.decks) - 1:
+    def check_sunk(self):
+        if self.decks.count(None) == len(self.decks) - 1:
             self.is_drowned = True
-            return self.is_drowned
+        return self.is_drowned
 
 
 class Deck:
