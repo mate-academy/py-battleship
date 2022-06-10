8bb8526f899e19801958a81404a57d159cf5157b
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-06-10 18:54:04 +0300

    Solution first part main part of task

--- app/main.py
+++ app/main.py
@@ -1,34 +1,39 @@
-class Deck:
-    def __init__(self, row, column, is_alive=True):
-        pass
+from typing import List
 
-
-class Ship:
-    def __init__(self, start, end, is_drowned=False):
-        # Create decks and save them to a list `self.decks`
-        pass
-
-    def get_deck(self, row, column):
-        # Find the corresponding deck in the list
-        pass
-
-    def fire(self, row, column):
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
-        pass
+from app.history_shoots import history_shoots
+from app.ship import Ship
 
 
 class Battleship:
-    def __init__(self, ships):
+    def __init__(self, ships: List[tuple]):
         # Create a dict `self.field`.
         # Its keys are tuples - the coordinates of the non-empty cells,
         # A value for each cell is a reference to the ship
         # which is located in it
-        pass
+        self.ships = ships
+        self.fleet = self._create_fleet()
 
+    def _create_fleet(self):
+        return {
+            cord: Ship(
+                start=cord[0],
+                end=cord[1]
+            )
+            for cord in self.ships
+        }
+
+    @history_shoots
     def fire(self, location: tuple):
         # This function should check whether the location
         # is a key in the `self.field`
         # If it is, then it should check if this cell is the last alive
         # in the ship or not.
-        pass
+        for ship in self.fleet.values():
+            deck = ship.get_deck(location[0], location[1])
+
+            if deck and not ship.chech_sunk():
+                return ship.hit_in_deck(deck)
+            elif deck and ship.chech_sunk():
+                return ship.sunk_ship(deck)
+
+        return ship.miss_in_ship()
