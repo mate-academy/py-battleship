988c3cbb860426aa1d2e1a89d336a6e1c26fcd5c
Vasyl Smutok <23.smutok@gmail.com>
2022-06-16 12:49:05 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -7,50 +7,47 @@ class Deck:
 
 class Ship:
     def __init__(self, start, end, is_drowned=False):
-        # Create decks and save them to a list `self.decks`
-        pass
+        self.is_drowned = is_drowned
+        self.decks = []
+
+        len_ship = abs(sum(start) - sum(end)) + 1
+        if start[0] == end[0]:
+            for i in range(len_ship):
+                self.decks.append(Deck(start[0], start[1] + i))
+        else:
+            for i in range(len_ship):
+                self.decks.append(Deck(start[0] + i, start[1]))
 
     def get_deck(self, row, column):
-        # Find the corresponding deck in the list
-        pass
+        for deck in self.decks:
+            if deck.row == row and deck.column == column:
+                return deck
 
     def fire(self, row, column):
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
-        pass
+        self.get_deck(row, column).is_alive = False
+
+        all_decks_is_alive = [deck.is_alive for deck in self.decks]
+        if not any(all_decks_is_alive):
+            self.is_drowned = True
 
 
 class Battleship:
-    dict_ships = {}
-    field = [["~" for _ in range(10)] for _ in range(10)]
-
     def __init__(self, ships):
+        self.field = {}
         for ship in ships:
-            location_ship = []
+            reference_to_ship = Ship(ship[0], ship[1])
             len_ship = abs(sum(ship[0]) - sum(ship[1])) + 1
             if ship[0][0] == ship[1][0]:
                 for i in range(len_ship):
-                    location_ship.append((ship[0][0], ship[0][1] + i,))
-                    self.field[ship[0][0]][ship[0][1] + i] = u"\u25A1"
+                    self.field[ship[0][0], ship[0][1] + i] = reference_to_ship
             else:
                 for i in range(len_ship):
-                    location_ship.append((ship[0][0] + i, ship[0][1],))
-                    self.field[ship[0][0] + i][ship[0][1]] = u"\u25A1"
-
-            self.dict_ships.update({tuple(location_ship): len_ship})
-
-    def print_info(self):
-        for column in self.field:
-            print(column)
+                    self.field[ship[0][0] + i, ship[0][1]] = reference_to_ship
 
     def fire(self, location: tuple):
-        for ship_coordinates in self.dict_ships.keys():
-            if location in ship_coordinates:
-                self.dict_ships[ship_coordinates] -= 1
-                if self.dict_ships[ship_coordinates] <= 0:
-                    self.field[location[0]][location[1]] = "x"
-                    return "Sunk!"
-
-                self.field[location[0]][location[1]] = "*"
-                return "Hit!"
-        return "Miss!"
+        if location not in self.field.keys():
+            return "Miss!"
+        self.field[location].fire(location[0], location[1])
+        if self.field[location].is_drowned is True:
+            return "Sunk!"
+        return "Hit!"
