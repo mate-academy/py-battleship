f57650d2efb3cc5a4ced826f256ea4347f4131e3
Vasyl Smutok <23.smutok@gmail.com>
2022-06-16 14:18:24 +0300

    created new method len_ship

--- app/main.py
+++ app/main.py
@@ -7,15 +7,16 @@ class Deck:
 
 class Ship:
     def __init__(self, start, end, is_drowned=False):
+        self.start = start
+        self.end = end
         self.is_drowned = is_drowned
         self.decks = []
 
-        len_ship = abs(sum(start) - sum(end)) + 1
         if start[0] == end[0]:
-            for i in range(len_ship):
+            for i in range(self.len_ship()):
                 self.decks.append(Deck(start[0], start[1] + i))
         else:
-            for i in range(len_ship):
+            for i in range(self.len_ship()):
                 self.decks.append(Deck(start[0] + i, start[1]))
 
     def get_deck(self, row, column):
@@ -30,18 +31,20 @@ class Ship:
         if not any(all_decks_is_alive):
             self.is_drowned = True
 
+    def len_ship(self):
+        return abs(sum(self.start) - sum(self.end)) + 1
+
 
 class Battleship:
     def __init__(self, ships):
         self.field = {}
         for ship in ships:
             reference_to_ship = Ship(ship[0], ship[1])
-            len_ship = abs(sum(ship[0]) - sum(ship[1])) + 1
             if ship[0][0] == ship[1][0]:
-                for i in range(len_ship):
+                for i in range(reference_to_ship.len_ship()):
                     self.field[ship[0][0], ship[0][1] + i] = reference_to_ship
             else:
-                for i in range(len_ship):
+                for i in range(reference_to_ship.len_ship()):
                     self.field[ship[0][0] + i, ship[0][1]] = reference_to_ship
 
     def fire(self, location: tuple):
