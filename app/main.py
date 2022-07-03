209da6deeafef7a3b34f635645ca6392188610cd
SenchaPencha <SenchaPencha@mail.ru>
2022-07-03 18:13:58 +0300

    Work

--- app/main.py
+++ app/main.py
@@ -1,12 +1,23 @@
 class Deck:
     def __init__(self, row, column, is_alive=True):
-        pass
+        self.position = (row, column)
+        self.is_alive = is_alive
 
 
 class Ship:
+
     def __init__(self, start, end, is_drowned=False):
+        self.decks = []
         # Create decks and save them to a list `self.decks`
-        pass
+        if start[0] == end[0]:
+            if start[1] == end[1]:
+                self.decks.append(Deck(start[0], start[1]))
+            else:
+                for i in range(end[1] - start[1] + 1):
+                    self.decks.append(Deck(start[0], start[1] + i))
+        else:
+            for i in range(end[0] - start[0] + 1):
+                self.decks.append(Deck(start[0] + i, start[1]))
 
     def get_deck(self, row, column):
         # Find the corresponding deck in the list
@@ -19,11 +30,15 @@ class Ship:
 
 
 class Battleship:
+    ships_dict = {}
+
     def __init__(self, ships):
         # Create a dict `self.field`.
         # Its keys are tuples - the coordinates of the non-empty cells,
         # A value for each cell is a reference to the ship
         # which is located in it
+        for ship in ships:
+            self.ships_dict[ship] = Ship(ship[0], end=ship[1])
         pass
 
     def fire(self, location: tuple):
@@ -31,4 +46,13 @@ class Battleship:
         # is a key in the `self.field`
         # If it is, then it should check if this cell is the last alive
         # in the ship or not.
-        pass
+        for ship in Battleship.ships_dict.values():
+            for deck in ship.decks:
+                if deck.position == location:
+                    deck.is_alive = False
+                    for deck_ in ship.decks:
+                        if deck_.is_alive:
+                            return "Hit!"
+                    return "Sunk!"
+        else:
+            return "Miss!"
