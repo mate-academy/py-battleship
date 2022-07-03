a0da8d957a9e4caa501c3bfaf75a73291acf0713
SenchaPencha <SenchaPencha@mail.ru>
2022-07-03 18:41:05 +0300

    Work

--- app/main.py
+++ app/main.py
@@ -5,11 +5,9 @@ class Deck:
 
 
 class Ship:
-
     def __init__(self, start, end, is_drowned=False):
         self.is_drowned = is_drowned
         self.decks = []
-        # Create decks and save them to a list `self.decks`
         if start[0] == end[0]:
             if start[1] == end[1]:
                 self.decks.append(Deck(start[0], start[1]))
@@ -21,14 +19,11 @@ class Ship:
                 self.decks.append(Deck(start[0] + i, start[1]))
 
     def get_deck(self, row, column):
-        # Find the corresponding deck in the list
         for deck in self.decks:
             if deck.position == (row, column):
                 return deck
 
     def fire(self, row, column):
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
         deck = self.get_deck(row, column)
         deck.is_alive = False
         for deck_ in self.decks:
@@ -41,10 +36,6 @@ class Ship:
 class Battleship:
 
     def __init__(self, ships):
-        # Create a dict `self.field`.
-        # Its keys are tuples - the coordinates of the non-empty cells,
-        # A value for each cell is a reference to the ship
-        # which is located in it
         self.field = {}
         for ship in ships:
             start = ship[0]
