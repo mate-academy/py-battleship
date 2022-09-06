8bbe5460b147bdab418e3ce50f090c2f2d64583e
stoveprofi <stoveprofi@gmail.com>
2022-09-06 11:03:45 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,34 +1,97 @@
 class Deck:
     def __init__(self, row, column, is_alive=True):
-        pass
+        self.row = row
+        self.column = column
+        self.is_alive = is_alive
 
 
 class Ship:
+
     def __init__(self, start, end, is_drowned=False):
-        # Create decks and save them to a list `self.decks`
-        pass
+        self.start = start
+        self.end = end
+        self.is_drowned = is_drowned
+        self.decks = []
+        self.decks_in_list = []
+        self.decks_list()
+
+    def decks_list(self):
+        if self.start[0] == self.end[0] and self.start[1] != self.end[1]:
+            for column_ in range(self.start[1], self.end[1] + 1):
+                self.decks.append(Deck(self.start[0], column_))
+        if self.start[1] == self.end[1] and self.start[0] != self.end[0]:
+            for row_ in range(self.start[0], self.end[0] + 1):
+                self.decks.append(Deck(row_, self.end[1]))
+        if self.start[0] == self.end[0] and self.start[1] == self.end[1]:
+            self.decks.append(Deck(self.start[0], self.end[1]))
+        return self.decks
 
     def get_deck(self, row, column):
-        # Find the corresponding deck in the list
-        pass
+        for object_ in self.decks:
+            if object_.row == row and object_.column == column:
+                return object_
 
     def fire(self, row, column):
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
-        pass
+        obj_deck = self.get_deck(row, column)
+        obj_deck.is_alive = False
+        self.decks_in_list.append(obj_deck)
+        if len(self.decks_in_list) == len(self.decks):
+            self.is_drowned = True
 
 
 class Battleship:
     def __init__(self, ships):
-        # Create a dict `self.field`.
-        # Its keys are tuples - the coordinates of the non-empty cells,
-        # A value for each cell is a reference to the ship
-        # which is located in it
-        pass
+        self.ships = ships
+        self.field = {}
+        self.battle_ship = None
+        self.field_battle()
+
+    def field_battle(self):
+        for ship in self.ships:
+            ship_obj = Ship(ship[0], ship[1])
+            if ship[0][0] == ship[1][0] and ship[0][1] != ship[1][1]:
+                for column_ in range(ship[0][1], ship[1][1] + 1):
+                    self.field[(ship[0][0], column_)] = ship_obj
+            if ship[0][1] == ship[1][1] and ship[0][0] != ship[1][0]:
+                for row_ in range(ship[0][0], ship[1][0] + 1):
+                    self.field[(row_, ship[0][1])] = ship_obj
+            if ship[0][0] == ship[1][0] and ship[0][1] == ship[1][1]:
+                self.field[(ship[0][0], ship[0][1])] = ship_obj
+        return self.field
 
     def fire(self, location: tuple):
-        # This function should check whether the location
-        # is a key in the `self.field`
-        # If it is, then it should check if this cell is the last alive
-        # in the ship or not.
-        pass
+        self.print_field()
+        if (location[0], location[1]) not in self.field:
+            return "Miss!"
+        ship_object = self.field[location]
+        ship_object.fire(location[0], location[1])
+        print(ship_object.is_drowned)
+        if (location[0], location[1]) in self.field and ship_object.is_drowned:
+            self.print_field()
+            print('\n'.join('\t'.join(map(str, row))
+                            for row in self.battle_ship))
+            return "Sunk!"
+        if (location[0], location[1]) in self.field and \
+                self.battle_ship[location[0]][location[1]] == u"\u25A1":
+            self.print_field()
+            print('\n'.join('\t'.join(map(str, row))
+                            for row in self.battle_ship))
+            return "Hit!"
+
+    def print_field(self):
+        battle_ = [['~'] * 10 for i in range(10)]
+        decks_on_field = []
+        for key in self.field:
+            decks_on_field.append(key)
+        for row, column in decks_on_field:
+            ship = self.field[(row, column)]
+            if ship.get_deck(row, column).is_alive:
+                battle_[row][column] = u"\u25A1"
+            if ship.is_drowned:
+                decks_ = ship.decks_list()
+                for deck_ in decks_:
+                    battle_[deck_.row][deck_.column] = "x"
+            elif not ship.get_deck(row, column).is_alive:
+                battle_[row][column] = "*"
+        self.battle_ship = battle_
+        return self.battle_ship
