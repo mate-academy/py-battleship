37d730b21b921f702f4f70f50b0022dca80f528f
mltdll <ilya.vyplavin@gmail.com>
2022-10-02 19:48:19 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,34 +1,129 @@
+from __future__ import annotations
+
+
 class Deck:
-    def __init__(self, row, column, is_alive=True):
-        pass
+    def __init__(self, row: int, column: int, is_alive: bool = True) -> None:
+        self.row = row
+        self.column = column
+        self.is_alive = is_alive
+
+    def get_hit(self):
+        self.is_alive = False
+
+    @property
+    def coords(self) -> tuple[int, int]:
+        return self.row, self.column
 
 
 class Ship:
-    def __init__(self, start, end, is_drowned=False):
-        # Create decks and save them to a list `self.decks`
-        pass
+    def __init__(
+        self,
+        start: tuple[int, int],
+        end: tuple[int, int],
+        is_drowned: bool = False,
+    ) -> None:
+        self.is_drowned = is_drowned
+        self.decks = [
+            Deck(row, column)
+            for row in range(start[0], end[0] + 1)
+            for column in range(start[1], end[1] + 1)
+        ]
 
-    def get_deck(self, row, column):
-        # Find the corresponding deck in the list
-        pass
+    def get_deck(self, row: int, column: int) -> Deck | None:
+        return next(
+            (deck for deck in self.decks if deck.coords == (row, column)), None
+        )
 
-    def fire(self, row, column):
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
-        pass
+    def fire(self, row: int, column: int) -> bool:
+        self.get_deck(row, column).get_hit()
+        return self._check_drowned()
+
+    @property
+    def size(self):
+        return len(self.decks)
+
+    def _check_drowned(self) -> bool:
+        if not any(deck.is_alive for deck in self.decks):
+            self.is_drowned = True
+
+        return self.is_drowned
 
 
 class Battleship:
-    def __init__(self, ships):
-        # Create a dict `self.field`.
-        # Its keys are tuples - the coordinates of the non-empty cells,
-        # A value for each cell is a reference to the ship
-        # which is located in it
-        pass
+    def __init__(
+        self,
+        ships: list[tuple[tuple[int, int], tuple[int, int]]],  # Wow.
+        field_size: tuple[int, int] = (10, 10),
+        ship_sizes: tuple[int] = (4, 3, 2, 1),
+    ) -> None:
+        self.ships = [Ship(ship[0], ship[1]) for ship in ships]
+        self.field = {
+            deck.coords: ship for ship in self.ships for deck in ship.decks
+        }
+        self.field_height, self.field_width = field_size
 
-    def fire(self, location: tuple):
-        # This function should check whether the location
-        # is a key in the `self.field`
-        # If it is, then it should check if this cell is the last alive
-        # in the ship or not.
-        pass
+        # Required amounts of ships by size.
+        self.ship_sizes = dict([*enumerate(ship_sizes, 1)])
+
+        self._validate_field()
+
+    def fire(self, location: tuple[int, int]) -> str:
+        if location not in self.field:
+            return "Miss!"
+
+        return "Sunk!" if self.field[location].fire(*location) else "Hit!"
+
+    def print_field(self):
+        for row in range(self.field_height):
+            row_cells = []
+            for column in range(self.field_width):
+                cell = (row, column)
+                if cell not in self.field:
+                    row_cells.append("~")
+                elif (ship := self.field[cell]).is_drowned:
+                    row_cells.append("☓")
+                else:
+                    row_cells.append(
+                        "□" if ship.get_deck(*cell).is_alive else "⊠"
+                    )
+            print(*row_cells, sep="  ")
+
+    def _validate_field(self) -> None:
+        if any(
+            not 0 <= deck[0] < self.field_height
+            or not 0 <= deck[1] < self.field_width
+            for deck in self.field
+        ):
+            raise ValueError("Some of the ships have invalid coordinates.")
+
+        if len(self.ships) != sum(self.ship_sizes.values()):
+            raise ValueError("Incorrect number of ships.")
+
+        if self._count_ships() != self.ship_sizes:
+            raise ValueError("Incorrect ship sizes.")
+
+        # Definitely refactorable.
+        # Unfortunately, I'm not in the mood right now...
+        coords = list(self.field.keys())
+        for i, coord in enumerate(coords[:-1], 1):
+            for other_coord in coords[i:]:
+                if (
+                    self.field[coord] is not self.field[other_coord]
+                    and abs(coord[0] - other_coord[0]) < 2
+                    and abs(coord[1] - other_coord[1]) < 2
+                ):
+                    raise ValueError(
+                        "Ships cannot overlap or be "
+                        "placed in neighboring cells"
+                    )
+
+    def _count_ships(self) -> dict:
+        ship_counter = {}
+
+        for ship in self.ships:
+            if ship.size in ship_counter:
+                ship_counter[ship.size] += 1
+            else:
+                ship_counter[ship.size] = 1
+
+        return ship_counter
