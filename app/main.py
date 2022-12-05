d4cf74e8398a81f237b09486fbc4508f2246ae3c
Andrey Polyakov <polyakovv.andrey.01@gmail.com>
2022-12-05 02:06:52 +0200

    Fixed bugs

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ class Deck:
 
 
 class Ship:
+
     def __init__(
             self,
             start: tuple,
@@ -15,17 +16,16 @@ class Ship:
         self.start = Deck(start[0], start[1])
         self.end = Deck(end[0], end[1])
         self.is_drowned = is_drowned
+        self.decks = self.create_decks()
+
+    def create_decks(self) -> list:
         diff_row = self.end.row - self.start.row
         diff_column = self.end.column - self.start.column
-        self.decks = [self.start]
-        if diff_row != 0:
-            for i in range(diff_row):
-                new_deck = Deck(self.start.row + (i + 1), self.start.column)
-                self.decks.append(new_deck)
-        if diff_column != 0:
-            for i in range(diff_column):
-                new_deck = Deck(self.start.row, self.start.column + (i + 1))
-                self.decks.append(new_deck)
+        return [Deck(self.start.row + i, self.start.column)
+                if diff_row != 0
+                else Deck(self.start.row, self.start.column + j)
+                for j in range(diff_column + 1)
+                for i in range(diff_row + 1)]
 
     def get_deck(self, row: int, column: int) -> Deck:
         for deck in self.decks:
@@ -36,21 +36,20 @@ class Ship:
         for deck in self.decks:
             if deck.row == row and deck.column == column:
                 deck.is_alive = False
-        dead_deck = 0
-        for deck in self.decks:
-            if deck.is_alive is False:
-                dead_deck += 1
-        if dead_deck == len(self.decks):
+        if sum([deck.is_alive for deck in self.decks]) == 0:
             self.is_drowned = True
 
 
 class Battleship:
     def __init__(self, ships: list[tuple[tuple[int]]]) -> None:
         self.ships = ships
+        self.field = {}
+        self.create_field()
+
+    def create_field(self) -> None:
         instances_of_ships = []
         for ship in self.ships:
             instances_of_ships.append(Ship(ship[0], ship[1]))
-        self.field = {}
         for ship_instance in instances_of_ships:
             for deck in ship_instance.decks:
                 self.field[(deck.row, deck.column)] = ship_instance
