3166e24eb46ae1ada525d2f5dcdeecfb9f303c0e
Admin <winofkill@gmail.com>
2022-12-06 18:57:25 +0200

    Done

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
 class Deck:
     battle_field = []
 
-    def __init__(self, row, column, is_alive=True):
+    def __init__(self, row: int, column: int, is_alive: bool = True) -> None:
         self.row = row
         self.column = column
-        self.battle_field = [["~" for _ in range(self.row)] for _ in range(self.column)]
+        self.battle_field = \
+            [["~" for _ in range(self.row)] for _ in range(self.column)]
         self.is_alive = is_alive
 
 
 class Ship:
-
-    def __init__(self, start, end, is_drowned=False):
+    def __init__(self, start: tuple,
+                 end: tuple,
+                 is_drowned: bool = False) -> None:
         self.decks = []
         self.start = start
         self.end = end
@@ -20,12 +22,12 @@ class Ship:
             for column in range(start[1], end[1] + 1):
                 self.decks.append(Deck(row, column))
 
-    def get_deck(self, row, column):
+    def get_deck(self, row: int, column: int) -> Deck:
         for deck in self.decks:
             if (deck.row, deck.column) == (row, column):
                 return deck
 
-    def fire(self, row, column):
+    def fire(self, row: int, column: int) -> None:
         self.get_deck(row, column).is_alive = False
         for deck in self.decks:
             self.is_drowned = True
@@ -35,7 +37,7 @@ class Ship:
 
 
 class Battleship:
-    def __init__(self, ships):
+    def __init__(self, ships: tuple) -> None:
         self.field = {}
         for ship in ships:
             battle_ship = Ship(ship[0], ship[1])
