1014cd03c28aef8088fd826ba4ed942318354dae
Andrew.H <john.riderr9@gmail.com>
2022-12-15 14:19:54 +0200

    removed comments

--- app/main.py
+++ app/main.py
@@ -61,31 +61,3 @@ class Battleship:
                 else:
                     print("~  ", end="")
             print("")
-
-
-ships = [
-    ((0, 0), (0, 3)),
-    ((0, 5), (0, 6)),
-    ((0, 8), (0, 9)),
-    ((2, 0), (4, 0)),
-    ((2, 4), (2, 6)),
-    ((2, 8), (2, 9)),
-    ((9, 9), (9, 9)),
-    ((7, 7), (7, 7)),
-    ((7, 9), (7, 9)),
-    ((9, 7), (9, 7)),
-]
-#
-# battle_ship = Battleship(ships)
-# battle_ship.print_field()
-#
-# print(
-#     battle_ship.fire((0, 4)),  # Miss!
-#     battle_ship.fire((0, 3)),  # Hit!
-#     battle_ship.fire((0, 2)),  # Hit!
-#     battle_ship.fire((0, 1)),  # Hit!
-#     battle_ship.fire((0, 0)),  # Sunk!
-#     battle_ship.fire((0, 5)),  # !
-#     battle_ship.fire((0, 5)),  # !
-# )
-# battle_ship.print_field()
