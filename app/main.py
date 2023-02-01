aa465b4b145bb79661c9b79fc821f0e9842c5db7
Oleksandr Mamchur <irex1988@gmail.com>
2023-02-01 23:42:27 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -34,70 +34,21 @@ class Ship:
         fired_deck = self.get_deck(row, column)
         if fired_deck:
             fired_deck.is_alive = False
-        print(self.decks)
         self.is_drowned = not any([deck.is_alive for deck in self.decks])
-        print("drowned", self.is_drowned)
 
 
 class Battleship:
     def __init__(self, ships: list) -> None:
         self.field = {ship: Ship(ship[0], ship[1]) for ship in ships}
-        self.ships = ships
-        # Create a dict `self.field`. Its keys are tuples - the coordinates
-        # of the non-empty cells, A value for each cell is a reference to the
-        # ship which is located in it
+        for key, value in self.field.items():
+            value.create_deck()
 
     def fire(self, location: tuple) -> str:
-        # This function should check whether the location is a key in the
-        # `self.field`. If it is, then it should check if this cell
-        # is the last alive in the ship or not.
         for key, value in self.field.items():
             if location[0] in range(key[0][0], key[1][0] + 1)\
                     and location[1] in range(key[0][1], key[1][1] + 1):
-                value.create_deck()
                 value.fire(location[0], location[1])
                 if value.is_drowned is True:
                     return "Sunk!"
                 return "Hit!"
         return "Miss!"
-
-
-    # def set_ships(self):
-    #     for ship in self.ships:
-    #
-    #         print(ship[0], ship[1])
-
-
-
-
-
-    # @staticmethod
-    # def battlefield() -> list:
-    #     return [["~" for _ in range(10)] for _ in range(10)]
-    #
-    # def print_field(self) -> None:
-    #     for row in self.location:
-    #         print(" ".join(row))
-
-
-
-
-battle_ship = Battleship(
-    ships=[
-        ((0, 0), (0, 3)),
-        ((0, 5), (0, 6)),
-        ((0, 8), (0, 9)),
-        ((2, 0), (4, 0)),
-        ((2, 4), (2, 6)),
-        ((2, 8), (2, 9)),
-        ((9, 9), (9, 9)),
-        ((7, 7), (7, 7)),
-        ((7, 9), (7, 9)),
-        ((9, 7), (9, 7)),
-    ]
-)
-
-print(battle_ship.fire((0, 4))) #miss
-print(battle_ship.fire((0, 5))) # == "Hit!"
-print(battle_ship.fire((0, 6))) #== sunk
-
