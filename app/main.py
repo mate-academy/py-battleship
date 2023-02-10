be064cb552fb1fece2e4a9ce31da4a58a21d5cdc
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2023-02-10 15:50:30 +0200

    Draft_1

--- app/main.py
+++ app/main.py
@@ -1,30 +1,47 @@
 class Deck:
     def __init__(self, row, column, is_alive=True):
-        pass
+        self.row = row
+        self.column = column
+        self.is_alive = is_alive
 
 
 class Ship:
     def __init__(self, start, end, is_drowned=False):
         # Create decks and save them to a list `self.decks`
-        pass
+        self.start = start
+        self.end = end
+        self.is_drowned = is_drowned
+        self.decks = []
+        for row in range(start[0], end[0] + 1):
+            for column in range(start[1], end[1] + 1):
+                self.decks.append(Deck(row, column))
 
     def get_deck(self, row, column):
         # Find the corresponding deck in the list
-        pass
+        for deck in self.decks:
+            if (deck.row, deck.column) == (row, column):
+                return deck
 
     def fire(self, row, column):
         # Change the `is_alive` status of the deck
         # And update the `is_drowned` value if it's needed
-        pass
+        self.get_deck(row, column).is_alive = False
+        for deck in self.decks:
+            if deck.is_alive:
+                return
+        self.is_drowned = True
 
 
 class Battleship:
-    def __init__(self, ships):
+    def __init__(self, ships: list):
         # Create a dict `self.field`.
         # Its keys are tuples - the coordinates of the non-empty cells,
         # A value for each cell is a reference to the ship
         # which is located in it
-        pass
+        self.ships = ships
+        self.field = {}
+
+
 
     def fire(self, location: tuple):
         # This function should check whether the location
