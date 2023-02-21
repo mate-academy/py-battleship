d9d75fe9a32763011f7a181571354fb60b266700
6aPa6aH <6apa6ah666@gmail.com>
2023-02-21 22:25:43 +0200

    fix_code

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
+from typing import Any
+
+
 class Deck:
-    def __init__(self, row: int, column: int, is_alive: bool = True) -> None:
+    def __init__(self, row: int,
+                 column: int,
+                 is_alive: bool = True) -> None:
         self.row = row
         self.column = column
         self.is_alive = is_alive
@@ -22,14 +27,14 @@ class Ship:
             for i in range(self.start[1], self.end[1] + 1):
                 self.decks.append(Deck(i, self.start[1]))
 
-    def get_deck(self, row: int, column: int) -> tuple:
+    def get_deck(self, row: int, column: int) -> Any:
         for deck in self.decks:
             if deck.row == row and deck.column == column:
                 return deck
 
     def fire(self, row: int, column: int) -> None:
         fired_deck = self.get_deck(row, column)
-        if fired_deck:
+        if fired_deck is not None:
             fired_deck.is_alive = False
         self.is_drowned = not any([deck.is_alive for deck in self.decks])
 
@@ -42,11 +47,11 @@ class Battleship:
             value.create_deck()
 
     def fire(self, location: tuple) -> str:
-        for key, value in self.field.items():
-            if (location[0] in range(key[0][0], key[1][0] + 1)
-                    and location[1] in range(key[0][1], key[1][1] + 1)):
-                value.fire(location[0], location[1])
-                if value.is_drowned is True:
+        for ship, deck in self.field.items():
+            if (location[0] in range(ship[0][0], ship[1][0] + 1)
+                    and location[1] in range(ship[0][1], ship[1][1] + 1)):
+                deck.fire(location[0], location[1])
+                if deck.is_drowned is True:
                     return "Sunk!"
                 return "Hit!"
         return "Miss!"
