ad68b72595da01133c84bba6ffc8a0dc06311958
dmytro <manjustice1998@gmail.com>
2023-02-22 11:49:17 +0200

    Added methods for creating ships and decks

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Dict
 
 
 class Deck:
@@ -21,13 +21,19 @@ class Ship:
             is_drowned: bool = False
     ) -> None:
         self.is_drowned = is_drowned
-        self.decks = []
+        self.decks = self._create_decks(start, end)
+
+    @staticmethod
+    def _create_decks(start: tuple, end: tuple) -> List[Deck]:
+        decks = []
         if start[0] == end[0]:
             for column in range(start[1], end[1] + 1):
-                self.decks.append(Deck(start[0], column))
+                decks.append(Deck(start[0], column))
         else:
             for row in range(start[0], end[0] + 1):
-                self.decks.append(Deck(row, start[1]))
+                decks.append(Deck(row, start[1]))
+
+        return decks
 
     def get_deck(self, row: int, column: int) -> Deck:
         for deck in self.decks:
@@ -43,11 +49,17 @@ class Ship:
 
 class Battleship:
     def __init__(self, ships: List[tuple]) -> None:
-        self.field = {}
+        self.field = self._create_ships(ships)
+
+    @staticmethod
+    def _create_ships(ships: List[tuple]) -> Dict[tuple, Ship]:
+        field = {}
         for start, end in ships:
             ship = Ship(start, end)
             key = tuple([(deck.row, deck.column)for deck in ship.decks])
-            self.field[key] = ship
+            field[key] = ship
+
+        return field
 
     def fire(self, location: tuple) -> str:
         for locs, ship in self.field.items():
