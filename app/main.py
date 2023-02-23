7ba3232530ba72aa032a98a915dc1d7454eeb09c
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-23 18:40:19 +0200

    refactoring

--- app/main.py
+++ app/main.py
@@ -1,17 +1,13 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
 
+
+@dataclass
 class Deck:
-    def __init__(self, row: int, column: int, is_alive: bool = True) -> None:
-        self.row = row
-        self.column = column
-        self.is_alive = is_alive
-
-    def __str__(self) -> str:
-        return f"deck({self.row}, {self.column})"
-
-    def __repr__(self) -> str:
-        return f"deck({self.row}, {self.column})"
+    row: int
+    column: int
+    is_alive: bool = True
 
 
 class Ship:
@@ -28,21 +24,15 @@ class Ship:
         self._add_to_decks()
         self.live_decks = len(self.decks)
 
-    def __str__(self) -> str:
-        return f"ship({self.start}, {self.end})"
-
-    def __repr__(self) -> str:
-        return f"ship(start: {self.start}, finish: {self.end})"
-
     def get_deck(self, row: int, column: int) -> bool:
         for deck in self.decks:
-            if deck.row == row and deck.column == column:
+            if (deck.row, deck.column) == (row, column):
                 return True
         return False
 
     def get_status(self, row: int, column: int) -> bool:
         for deck in self.decks:
-            if deck.row == row and deck.column == column and deck.is_alive:
+            if self.get_deck(row, column) and deck.is_alive:
                 return True
         return False
 
@@ -52,13 +42,11 @@ class Ship:
 
         if start_x != end_x:
             for i in range(start_x, end_x + 1):
-                start_x = i
-                self.decks.append(Deck(start_x, start_y))
+                self.decks.append(Deck(i, start_y))
 
         elif start_y != end_y:
             for i in range(start_y, end_y + 1):
-                start_y = i
-                self.decks.append(Deck(start_x, start_y))
+                self.decks.append(Deck(start_x, i))
 
         else:
             self.decks.append(Deck(start_x, start_y))
@@ -69,14 +57,13 @@ class Battleship:
             self,
             ships: list[tuple[tuple[int, int], tuple[int, int]]]
     ) -> None:
-        self.ships = {}
-        self._add_ships_to_dict(ships)
+        self.ships = {ship: Ship(ship[0], ship[1]) for ship in ships}
 
     def fire(self, location: tuple) -> str:
         x, y = location[0], location[1]
         for ship in self.ships.values():
             for deck in ship.decks:
-                if deck.row == x and deck.column == y:
+                if (deck.row, deck.column) == (x, y):
                     deck.is_alive = False
                     ship.live_decks -= 1
                     if ship.live_decks == 0:
@@ -85,13 +72,6 @@ class Battleship:
                     return "Hit!"
         else:
             return "Miss!"
-
-    def _add_ships_to_dict(
-            self,
-            ships: list[tuple[tuple[int, int], tuple[int, int]]]
-    ) -> None:
-        for ship in ships:
-            self.ships[ship] = Ship(ship[0], ship[1])
 
     def draw_matrix(self, rows: int, columns: int) -> str:
         result = ""
