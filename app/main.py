1c19f80c84a292f3be9fe51b70c9e4e5f5077977
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-23 18:57:33 +0200

    refactoring

--- app/main.py
+++ app/main.py
@@ -20,8 +20,9 @@ class Ship:
         self.start = start
         self.end = end
         self.is_drowned = is_drowned
-        self.decks = []
-        self._add_to_decks()
+        self.decks = [Deck(row, column)
+                      for row in range(start[0], end[0] + 1)
+                      for column in range(start[1], end[1] + 1)]
         self.live_decks = len(self.decks)
 
     def get_deck(self, row: int, column: int) -> bool:
@@ -36,31 +37,16 @@ class Ship:
                 return True
         return False
 
-    def _add_to_decks(self) -> None:
-        start_x, start_y = self.start[0], self.start[1]
-        end_x, end_y = self.end[0], self.end[1]
-
-        if start_x != end_x:
-            for i in range(start_x, end_x + 1):
-                self.decks.append(Deck(i, start_y))
-
-        elif start_y != end_y:
-            for i in range(start_y, end_y + 1):
-                self.decks.append(Deck(start_x, i))
-
-        else:
-            self.decks.append(Deck(start_x, start_y))
-
 
 class Battleship:
     def __init__(
             self,
             ships: list[tuple[tuple[int, int], tuple[int, int]]]
     ) -> None:
-        self.ships = {ship: Ship(ship[0], ship[1]) for ship in ships}
+        self.ships = {ship: Ship(*ship) for ship in ships}
 
     def fire(self, location: tuple) -> str:
-        x, y = location[0], location[1]
+        x, y = location
         for ship in self.ships.values():
             for deck in ship.decks:
                 if (deck.row, deck.column) == (x, y):
