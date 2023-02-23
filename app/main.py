9fbe4b2417f68d0a084cea527aa309769acdcc77
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-23 15:41:34 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,34 +1,111 @@
+from __future__ import annotations
+
+
+class DeckNotInShip(Exception):
+    pass
+
+
 class Deck:
-    def __init__(self, row, column, is_alive=True):
-        pass
+    def __init__(self, row: int, column: int, is_alive: bool = True) -> None:
+        self.row = row
+        self.column = column
+        self.is_alive = is_alive
+
+    def __str__(self) -> str:
+        return f"deck({self.row}, {self.column})"
+
+    def __repr__(self) -> str:
+        return f"deck({self.row}, {self.column})"
+
+    @staticmethod
+    def draw_matrix(rows: int, columns: int) -> list:
+        result = []
+        for column in range(columns):
+            for row in range(rows):
+                deck = Deck(column, row)
+                result.append(deck)
+        return result
 
 
 class Ship:
-    def __init__(self, start, end, is_drowned=False):
-        # Create decks and save them to a list `self.decks`
-        pass
+    def __init__(
+            self,
+            start: tuple[int, int],
+            end: tuple[int, int],
+            is_drowned: bool = False
+    ) -> None:
+        self.start = start
+        self.end = end
+        self.is_drowned = is_drowned
+        self.decks = []
+        self._add_to_decks()
+        self.live_decks = len(self.decks)
 
-    def get_deck(self, row, column):
-        # Find the corresponding deck in the list
-        pass
+    def __str__(self) -> str:
+        return f"ship({self.start}, {self.end})"
 
-    def fire(self, row, column):
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
-        pass
+    def __repr__(self) -> str:
+        return f"ship(start: {self.start}, finish: {self.end})"
+
+    def get_deck(self, row: int, column: int) -> str | Exception:
+        for deck in self.decks:
+            if deck.row == row and deck.column == column:
+                return f"{deck} in this ship"
+        else:
+            raise DeckNotInShip(
+                f"deck({row}, {column}) doesn't belong this ship")
+
+    def fire(self, row: int, column: int) -> bool:
+        for deck in self.decks:
+            if deck.row == row and deck.column == column:
+                deck.is_alive = False
+                self.live_decks -= 1
+        if self.live_decks == 0:
+            self.is_drowned = True
+        return self.is_drowned
+
+    def _add_to_decks(self) -> None:
+        start_x, start_y = self.start[0], self.start[1]
+        end_x, end_y = self.end[0], self.end[1]
+
+        if start_x != end_x:
+            for i in range(start_x, end_x + 1):
+                start_x = i
+                self.decks.append(Deck(start_x, start_y))
+
+        elif start_y != end_y:
+            for i in range(start_y, end_y + 1):
+                start_y = i
+                self.decks.append(Deck(start_x, start_y))
+
+        else:
+            self.decks.append(Deck(start_x, start_y))
 
 
 class Battleship:
-    def __init__(self, ships):
-        # Create a dict `self.field`.
-        # Its keys are tuples - the coordinates of the non-empty cells,
-        # A value for each cell is a reference to the ship
-        # which is located in it
-        pass
+    def __init__(
+            self,
+            ships: list[tuple[tuple[int, int], tuple[int, int]]]
+    ) -> None:
+        self.ships = {}
+        self._add_ships_to_dict(ships)
 
-    def fire(self, location: tuple):
-        # This function should check whether the location
-        # is a key in the `self.field`
-        # If it is, then it should check if this cell is the last alive
-        # in the ship or not.
-        pass
+    def fire(self, location: tuple) -> str:
+        x, y = location[0], location[1]
+        for ship in self.ships.values():
+            for deck in ship.decks:
+                if deck.row == x and deck.column == y:
+                    deck.is_alive = False
+                    ship.live_decks -= 1
+                    if ship.live_decks == 0:
+                        return "Sunk!"
+                    return "Hit!"
+        else:
+            return "Miss!"
+
+    def _add_ships_to_dict(
+            self,
+            ships: list[tuple[tuple[int, int], tuple[int, int]]]
+    ) -> None:
+        for ship in ships:
+            self.ships[ship] = Ship(ship[0], ship[1])
