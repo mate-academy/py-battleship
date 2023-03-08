d3e5f746189c8a96d190d987e4819f74969ad5c5
kuznetsb <kuznetsbog@gmail.com>
2023-03-08 21:36:08 +0200

    Solution with changes

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
+from typing import List
+
+
+class ShipsQuantityError(Exception):
+    """Error that handles possible quantity of ships"""
+
+
 class Deck:
     def __init__(self, row: int, column: int, is_alive: bool = True) -> None:
         self.row = row
         self.column = column
         self.is_alive = is_alive
+
+    def __repr__(self) -> str:
+        return "x"
+
+    def __str__(self) -> str:
+        if self.is_alive:
+            return "□"
+        return "*"
 
 
 class Ship:
@@ -12,17 +27,16 @@ class Ship:
         self.is_drowned = is_drowned
         self.decks = []
         if self.end_row - self.start_row > 0:
-            for i in range(self.start_row, self.end_row + 1):
-                self.decks.append(Deck(i, self.start_column))
+            for row_cell in range(self.start_row, self.end_row + 1):
+                self.decks.append(Deck(row_cell, self.start_column))
         else:
-            for i in range(self.start_column, self.end_column + 1):
-                self.decks.append(Deck(self.start_row, i))
+            for column_cell in range(self.start_column, self.end_column + 1):
+                self.decks.append(Deck(self.start_row, column_cell))
 
     def get_deck(self, row: int, column: int) -> tuple | None:
         for deck in self.decks:
             if row == deck.row and column == deck.column:
                 return row, column
-        return
 
     def fire(self, row: int, column: int) -> None:
         for deck in self.decks:
@@ -34,15 +48,78 @@ class Ship:
 
 
 class Battleship:
-    def __init__(self, ships: tuple[tuple]) -> None:
+    FIELD_SIZE = 10
+
+    def __init__(self, ships: List[tuple]) -> None:
         self.field = {ship: Ship(ship[0], ship[1]) for ship in ships}
+        self._validate_input()
 
     def fire(self, location: tuple) -> str:
-        for ship in self.field:
-            deck = self.field[ship].get_deck(location[0], location[1])
+        for ship in self.field.values():
+            deck = ship.get_deck(location[0], location[1])
             if deck:
-                self.field[ship].fire(deck[0], deck[1])
-                if self.field[ship].is_drowned:
+                ship.fire(deck[0], deck[1])
+                if ship.is_drowned:
                     return "Sunk!"
                 return "Hit!"
         return "Miss!"
+
+    def _validate_input(self) -> None:
+        if len(self.field) != 10:
+            raise ShipsQuantityError(f"Total amount of ships must be 10, "
+                                     f"not {len(self.field)}")
+        decks = {
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0
+        }
+        limitations = {
+            1: 4,
+            2: 3,
+            3: 2,
+            4: 1
+        }
+        for ship in self.field.values():
+            decks[len(ship.decks)] += 1
+        for ship, quantity in decks.items():
+            if limitations[ship] != quantity:
+                raise ShipsQuantityError(f"There should be {limitations[ship]}"
+                                         f" {ship}-deck ships, not {quantity}")
+
+    def print_field(self) -> None:
+        field = [
+            ["~" for _ in range(Battleship.FIELD_SIZE)]
+            for _ in range(Battleship.FIELD_SIZE)
+        ]
+        for ship in self.field.values():
+            for deck in ship.decks:
+                if ship.is_drowned:
+                    field[deck.row][deck.column] = repr(deck)
+                else:
+                    field[deck.row][deck.column] = str(deck)
+        for row in field:
+            print(*row)
+
+
+battle_ship = Battleship(
+    ships=[
+        ((0, 0), (0, 3)),
+        ((0, 5), (0, 6)),
+        ((0, 8), (0, 9)),
+        ((2, 0), (4, 0)),
+        ((2, 4), (2, 6)),
+        ((2, 8), (2, 9)),
+        ((9, 9), (9, 9)),
+        ((7, 7), (7, 7)),
+        ((7, 9), (7, 9)),
+        ((9, 7), (9, 7)),
+    ]
+)
+battle_ship.fire((0, 4)),  # Miss!
+battle_ship.fire((0, 3)),  # Hit!
+battle_ship.fire((0, 2)),  # Hit!
+battle_ship.fire((0, 1)),  # Hit!
+battle_ship.fire((0, 0)),  # Sunk!
+
+battle_ship.print_field()
