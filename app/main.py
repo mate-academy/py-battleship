6ecff1aa3fce1b63bd5877e0dba947a4301a3a81
kuznetsb <kuznetsbog@gmail.com>
2023-03-09 22:05:12 +0200

    My checks removed

--- app/main.py
+++ app/main.py
@@ -100,26 +100,3 @@ class Battleship:
                     field[deck.row][deck.column] = str(deck)
         for row in field:
             print(*row)
-
-
-battle_ship = Battleship(
-    ships=[
-        ((0, 0), (0, 3)),
-        ((0, 5), (0, 6)),
-        ((0, 8), (0, 9)),
-        ((2, 0), (4, 0)),
-        ((2, 4), (2, 6)),
-        ((2, 8), (2, 9)),
-        ((9, 9), (9, 9)),
-        ((7, 7), (7, 7)),
-        ((7, 9), (7, 9)),
-        ((9, 7), (9, 7)),
-    ]
-)
-battle_ship.fire((0, 4)),  # Miss!
-battle_ship.fire((0, 3)),  # Hit!
-battle_ship.fire((0, 2)),  # Hit!
-battle_ship.fire((0, 1)),  # Hit!
-battle_ship.fire((0, 0)),  # Sunk!
-
-battle_ship.print_field()
