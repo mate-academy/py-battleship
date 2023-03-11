7ce8574fa2d214515ed9f67fa42cd5fd9bb0adaa
Arsen Makovei <makoveyarsen@gmail.com>
2023-03-11 13:09:31 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -15,10 +15,11 @@ class Ship:
         self.start = start
         self.end = end
         self.is_drowned = is_drowned
-        self.decks = []
-        for row in range(start[0], end[0] + 1):
-            for column in range(start[1], end[1] + 1):
-                self.decks.append(Deck(row, column))
+        self.decks = [
+            Deck(row, column)
+            for column in range(start[1], end[1] + 1)
+            for row in range(start[0], end[0] + 1)
+        ]
 
     def get_deck(self, row: int, column: int) -> Deck:
         for deck in self.decks:
