f61d4d6379a4ac12113a37db4bbc6b1facda714e
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-03-24 17:01:48 +0200

    Implement set_decks_with_margin_cells()

--- app/main.py
+++ app/main.py
@@ -22,7 +22,12 @@ class Ship:
         self.is_drowned = is_drowned
 
         self.decks = []
+        self.set_decks()
 
+        self.decks_with_margin_cells = set()
+        self.set_decks_with_margin_cells()
+
+    def set_decks(self):
         if self.start[0] != self.end[0]:
             for i in range(self.start[0], self.end[0] + 1):
                 self.decks.append(Deck(i, self.start[1]))
@@ -33,6 +38,101 @@ class Ship:
 
         if self.start[0] == self.end[0] and self.start[1] == self.end[1]:
             self.decks.append(Deck(self.start[0], self.start[1]))
+
+    def set_decks_with_margin_cells(self):
+        for deck in self.decks:
+            if deck.row == 0 and deck.column == 0:
+                self.decks_with_margin_cells.update([
+                    (deck.row, deck.column),
+                    (deck.row, deck.column + 1),
+                    (deck.row + 1, deck.column),
+                    (deck.row + 1, deck.column + 1),
+                ])
+            elif deck.row == 0 and deck.column == 9:
+                self.decks_with_margin_cells.update([
+                    (deck.row, deck.column),
+                    (deck.row, deck.column - 1),
+                    (deck.row + 1, deck.column - 1),
+                    (deck.row + 1, deck.column),
+                ])
+            elif deck.row == 9 and deck.column == 0:
+                self.decks_with_margin_cells.update([
+                    (deck.row, deck.column),
+                    (deck.row - 1, deck.column),
+                    (deck.row - 1, deck.column + 1),
+                    (deck.row, deck.column + 1),
+                ])
+            elif deck.row == 9 and deck.column == 9:
+                self.decks_with_margin_cells.update([
+                    (deck.row, deck.column),
+                    (deck.row - 1, deck.column),
+                    (deck.row - 1, deck.column - 1),
+                    (deck.row, deck.column - 1),
+                ])
+            elif (
+                    deck.row == 0
+                    and deck.column != 0
+                    and deck.column != 9
+            ):
+                self.decks_with_margin_cells.update([
+                    (deck.row, deck.column),
+                    (deck.row, deck.column + 1),
+                    (deck.row + 1, deck.column),
+                    (deck.row + 1, deck.column + 1),
+                    (deck.row, deck.column - 1),
+                    (deck.row + 1, deck.column - 1),
+                ])
+            elif (
+                    deck.row == 9
+                    and deck.column != 0
+                    and deck.column != 9
+            ):
+                self.decks_with_margin_cells.update([
+                    (deck.row, deck.column),
+                    (deck.row, deck.column - 1),
+                    (deck.row - 1, deck.column - 1),
+                    (deck.row - 1, deck.column),
+                    (deck.row - 1, deck.column + 1),
+                    (deck.row, deck.column + 1),
+                ])
+            elif (
+                    deck.column == 0
+                    and deck.row != 0
+                    and deck.row != 9
+            ):
+                self.decks_with_margin_cells.update([
+                    (deck.row, deck.column),
+                    (deck.row - 1, deck.column),
+                    (deck.row - 1, deck.column + 1),
+                    (deck.row, deck.column + 1),
+                    (deck.row + 1, deck.column + 1),
+                    (deck.row + 1, deck.column),
+                ])
+            elif (
+                    deck.column == 9
+                    and deck.row != 0
+                    and deck.row != 9
+            ):
+                self.decks_with_margin_cells.update([
+                    (deck.row, deck.column),
+                    (deck.row - 1, deck.column),
+                    (deck.row - 1, deck.column - 1),
+                    (deck.row, deck.column - 1),
+                    (deck.row + 1, deck.column - 1),
+                    (deck.row + 1, deck.column),
+                ])
+            else:
+                self.decks_with_margin_cells.update([
+                    (deck.row, deck.column),
+                    (deck.row - 1, deck.column),
+                    (deck.row - 1, deck.column + 1),
+                    (deck.row, deck.column + 1),
+                    (deck.row + 1, deck.column + 1),
+                    (deck.row + 1, deck.column),
+                    (deck.row + 1, deck.column - 1),
+                    (deck.row, deck.column - 1),
+                    (deck.row - 1, deck.column - 1),
+                ])
 
     def get_deck(self, row, column):
         # Find the corresponding deck in the list
@@ -62,9 +162,6 @@ class Battleship:
             for deck in ship.decks:
                 self.field[(deck.row, deck.column)] = ship
 
-    def __validate_field(self):
-        pass
-
     def fire(self, location: tuple):
         # This function should check whether the location
         # is a key in the `self.field`
@@ -80,6 +177,9 @@ class Battleship:
             return "Hit!"
 
         return "Miss!"
+
+    def _validate_field(self):
+        pass
 
     def print_field(self):
         field_matrix = []
@@ -139,3 +239,5 @@ battle_ship.print_field()
 print(battle_ship.fire((0, 0)))  # Sunk!
 battle_ship.print_field()
 
+for ship in battle_ship.ships:
+    print(ship.decks_with_margin_cells)
