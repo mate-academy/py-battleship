32edfa47a82c27260461c53cb267d30a969d07e7
Bohdan2001007 <bogdan1991007@gmail.com>
2023-03-25 17:07:47 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from typing import Tuple, List, Union
 
 
 class Deck:
-    def __init__(self, row: int, column: int, is_alive: bool = True):
+    def __init__(self, row: int, column: int, is_alive: bool = True) -> None:
         self.row = row
         self.column = column
         self.is_alive = is_alive
