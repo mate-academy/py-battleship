f6c0d34f44c924a3d3b1a195222496139d40ed6f
Andy <andy77andy@ukr.net>
2023-03-26 17:13:59 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,34 +1,44 @@
 class Deck:
-    def __init__(self, row, column, is_alive=True):
-        pass
+    def __init__(self, row: int, column: int, is_alive=True):
+        self.row = row
+        self.column = column
+        self.is_alive = True
 
 
 class Ship:
     def __init__(self, start, end, is_drowned=False):
-        # Create decks and save them to a list `self.decks`
-        pass
+        self.start = start
+        self.end = end
+        self.decks = []
+        self.is_drowned = False
 
-    def get_deck(self, row, column):
-        # Find the corresponding deck in the list
-        pass
+    def get_deck(self, row, column) -> list[Deck]:
+        decks = []
+        if self.start[0] == self.end[0]:
+            for column in range(self.start[1], self.end[1] + 1):
+                decks.append(Deck(self.start[0], column))
+        else:
+            for row in range(self.start[0], self.end[0] + 1):
+                decks.append(Deck(row, self.start[1]))
+        return decks
 
     def fire(self, row, column):
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
-        pass
+        for deck in self.decks:
+            if deck[0] == row and deck[1] == column:
+                deck.is_alive = False
+        if len([deck.is_alive for deck in self.decks]) == 0:
+            self.is_drowned = True
 
 
 class Battleship:
-    def __init__(self, ships):
-        # Create a dict `self.field`.
-        # Its keys are tuples - the coordinates of the non-empty cells,
-        # A value for each cell is a reference to the ship
-        # which is located in it
-        pass
+    def __init__(self, ships: list[tuple]) -> None:
+        self.field = {ship: Ship(*ship) for ship in ships}
 
     def fire(self, location: tuple):
-        # This function should check whether the location
-        # is a key in the `self.field`
-        # If it is, then it should check if this cell is the last alive
-        # in the ship or not.
-        pass
+        for ship in self.field:
+            if location in ship:
+                Ship(*ship).fire(*location)
+                if Ship(*ship).is_drowned:
+                    return "Sunk!"
+                return "Hit!"
+            return "Miss!"
