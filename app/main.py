cafdffa7f7744af08a9dfedbbf491a8da41595cc
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-03-26 12:40:56 +0300

    Move Battleship class to main.py

--- app/main.py
+++ app/main.py
@@ -1,25 +1,74 @@
-from app.battleship import Battleship
+from app.ship import Ship
 
 
-battle_ship = Battleship(
-    ships=[
-        ((0, 0), (0, 3)),
-        ((0, 5), (0, 6)),
-        ((0, 8), (0, 9)),
-        ((2, 0), (4, 0)),
-        ((2, 4), (2, 6)),
-        ((2, 8), (2, 9)),
-        ((9, 9), (9, 9)),
-        ((7, 7), (7, 7)),
-        ((7, 9), (7, 9)),
-        ((9, 7), (9, 7)),
-    ]
-)
+class Battleship:
+    def __init__(
+            self,
+            ships: list[tuple[tuple[int, int], tuple[int, int]]]
+    ) -> None:
+        self.ships: list[Ship] = []
+        self.field = {}
 
-print(
-    battle_ship.fire((0, 4)),  # Miss!
-    battle_ship.fire((0, 3)),  # Hit!
-    battle_ship.fire((0, 2)),  # Hit!
-    battle_ship.fire((0, 1)),  # Hit!
-    battle_ship.fire((0, 0)),  # Sunk!
-)
+        for ship in ships:
+            ship_instance = Ship(ship[0], ship[1])
+            self.ships.append(ship_instance)
+            for deck in ship_instance.decks:
+                self.field[(deck.row, deck.column)] = ship_instance
+
+        self._validate_field()
+
+    def fire(self, location: tuple) -> str:
+        if location in self.field:
+            ship = self.field.get(location)
+            ship.fire(location[0], location[1])
+
+            if ship.is_drowned:
+                return "Sunk!"
+
+            return "Hit!"
+
+        return "Miss!"
+
+    def _validate_field(self) -> None:
+        if len(self.ships) != 10:
+            raise Exception("Number of ships must be 10")
+        if sum(len(ship.decks) for ship in self.ships) != 20:
+            raise Exception("There must be 4 single-deck ships, "
+                            "3 double-deck ships, 2 three-deck ships "
+                            "and 1 four-deck ship")
+        for i in range(len(self.ships)):
+            ship = self.ships[i]
+
+            for count in range(i + 1, len(self.ships)):
+
+                if len(ship.decks_with_margin_cells.intersection(
+                    self.ships[count].decks_cells
+                )) != 0:
+                    raise Exception("Ships shouldn't be located "
+                                    "in the neighboring cells "
+                                    "(even if cells are neighbors "
+                                    "by diagonal)")
+
+    def print_field(self) -> None:
+        field_matrix = []
+
+        for _ in range(10):
+            row_items = ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"]
+            field_matrix.append(row_items)
+
+        for location in self.field:
+            ship = self.field.get(location)
+
+            if ship.is_drowned:
+                field_matrix[location[0]][location[1]] = "X"
+
+            else:
+                deck = ship.get_deck(location[0], location[1])
+
+                if deck.is_alive:
+                    field_matrix[location[0]][location[1]] = u"\u25A1"
+                else:
+                    field_matrix[location[0]][location[1]] = "*"
+
+        print("\n".join(["".join(["{:4}".format(item) for item in row])
+                         for row in field_matrix]))
