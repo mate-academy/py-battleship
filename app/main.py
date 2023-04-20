928f32d78df249f305949ee5371f27f081f6fcea
“Ruslan <callogan217@gmail.com>
2023-04-20 15:08:46 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,34 +1,157 @@
+from typing import Optional
+
+
 class Deck:
-    def __init__(self, row, column, is_alive=True):
-        pass
+    def __init__(self, row: int, col: int, is_alive: bool = True) -> None:
+        self.row = row
+        self.col = col
+        self.is_alive = is_alive
+
+    def __repr__(self) -> str:
+        return f"Deck(row={self.row}, col={self.col}, " \
+               f"is_alive={self.is_alive})"
 
 
 class Ship:
-    def __init__(self, start, end, is_drowned=False):
-        # Create decks and save them to a list `self.decks`
-        pass
+    def __init__(
+            self,
+            start: tuple[int, int],
+            end: tuple[int, int],
+            is_drowned: bool = False
+    ) -> None:
+        self.is_drowned = is_drowned
+        self._initialize_decks(start, end)
 
-    def get_deck(self, row, column):
-        # Find the corresponding deck in the list
-        pass
+    def _initialize_decks(
+            self,
+            start: tuple[int, int],
+            end: tuple[int, int]
+    ) -> None:
+        if start[1] != end[1]:
+            self.decks = [Deck(start[0], index)
+                          for index in range(start[1], end[1] + 1)]
+        elif start[0] != end[0]:
+            self.decks = [Deck(index, end[1])
+                          for index in range(start[0], end[0] + 1)]
+        else:
+            self.decks = [Deck(start[0], end[1])
+                          for index in range(start[0], end[0] + 1)]
 
-    def fire(self, row, column):
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
-        pass
+    def location(self) -> list:
+        return [(deck.row, deck.column) for deck in self.decks]
+
+    def get_deck(self, row: int, col: int) -> Optional[Deck]:
+        return next((deck for deck in self.decks
+                     if deck.row == row and deck.col == col), None)
+
+    def fire(self, row: int, col: int) -> None:
+        deck = self.get_deck(row, col)
+        if deck:
+            deck.is_alive = False
+            if all(not deck.is_alive for deck in self.decks):
+                self.is_drowned = True
+
+    def __repr__(self) -> str:
+        return f"Ship(decks={self.decks}, is_drowned={self.is_drowned})"
 
 
 class Battleship:
-    def __init__(self, ships):
-        # Create a dict `self.field`.
-        # Its keys are tuples - the coordinates of the non-empty cells,
-        # A value for each cell is a reference to the ship
-        # which is located in it
-        pass
+    def __init__(self, ships: Ship) -> None:
+        self.field = {}
+        for start, end in ships:
+            ship = Ship(start, end)
+            for deck in ship.decks:
+                self.field[(deck.row, deck.col)] = ship
 
-    def fire(self, location: tuple):
-        # This function should check whether the location
-        # is a key in the `self.field`
-        # If it is, then it should check if this cell is the last alive
-        # in the ship or not.
-        pass
+    def fire(self, location: list) -> str:
+        if location in self.field:
+            ship = self.field[location]
+            ship.fire(location[0], location[1])
+            if ship.is_drowned:
+                return "Sunk!"
+            else:
+                return "Hit!"
+        else:
+            return "Miss!"
+
+
+class GameBoard:
+    EMPTY_SQUARE = "\t\t~"
+    HIT_SQUARE = "\t\tx"
+    SHIP_SQUARE = "\t\t\u25A1"
+    MISS_SQUARE = "\t\t*"
+
+    def __init__(self) -> None:
+        self.field = self._create_empty_field()
+
+    def _create_empty_field(self) -> list:
+        return [[self.EMPTY_SQUARE] * 10 for _ in range(10)]
+
+    def print_field(self) -> None:
+        field = self._create_empty_field()
+        for ship in self.field.values():
+            points = ship.location()
+            for row, column in points:
+                if ship.is_drowned:
+                    field[row][column] = self.HIT_SQUARE
+                elif ship.get_deck(row, column):
+                    field[row][column] = self.SHIP_SQUARE
+                else:
+                    field[row][column] = self.MISS_SQUARE
+        for row in field:
+            print("".join(row))
+
+
+class PrintField:
+    EMPTY_SQUARE = "\t\t~"
+    HIT_SQUARE = "\t\tx"
+    SHIP_SQUARE = "\t\t\u25A1"
+    MISS_SQUARE = "\t\t*"
+
+    def __init__(self) -> None:
+        self.field = self._create_empty_field()
+
+    def _create_empty_field(self) -> list:
+        return [[self.EMPTY_SQUARE] * 10 for _ in range(10)]
+
+    def print_field(self) -> None:
+        field = self._create_empty_field()
+        for ship in self.field.values():
+            points = ship.location()
+            for row, column in points:
+                if ship.is_drowned:
+                    field[row][column] = self.HIT_SQUARE
+                elif ship.get_deck(row, column):
+                    field[row][column] = self.SHIP_SQUARE
+                else:
+                    field[row][column] = self.MISS_SQUARE
+        for row in field:
+            print("".join(row))
+
+
+class ValidateInput:
+    SHIP_DECKS = {1: 4, 2: 3, 3: 2, 4: 1}
+    SHIP_SIZE = {1: 1, 2: 2, 3: 3, 4: 4}
+
+    def __init__(self) -> None:
+        self.field = self._create_empty_field()
+        self._validate_field()
+
+    def _validate_field(self) -> None:
+        total_ships = sum(self.SHIP_DECKS.values())
+        if total_ships != 10:
+            raise ValueError(f"Expected 10 ships, got {total_ships}")
+
+        for size, count in self.SHIP_DECKS.items():
+            if count != sum(ship.get_size() == size
+                            for ship in self.field.values()):
+                raise ValueError(f"Expected {count} "
+                                 f"{self.SHIP_SIZE[size]}-deck ships")
+
+        for ship in self.field.values():
+            for row, col in ship.location():
+                for r_offset in [-1, 0, 1]:
+                    for c_offset in [-1, 0, 1]:
+                        if (r_offset != 0 or c_offset != 0) and \
+                                self.field.get((row + r_offset, col + c_offset)):
+                            raise ValueError("Ships cannot be adjacent")
