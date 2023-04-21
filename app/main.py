06026161b1e6dec14f396c8f0c05d2b342ae80d2
Oleksandr Yanchuk <sashajanchuck13@gmail.com>
2023-04-21 10:05:45 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,8 +4,9 @@ class Deck:
     def __init__(self, row: int, column: int, is_alive: bool = True) -> None:
         self.row = row
         self.column = column
-        self.battle_field = \
-            [["~" for _ in range(self.row)] for _ in range(self.column)]
+        self.battle_field = [
+            ["~" for _ in range(self.row)] for _ in range(self.column)
+        ]
         self.is_alive = is_alive
 
 
