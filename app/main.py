cf8c9518648277d6671db402a1a8eef5536ffb5b
Mykola <nickkozlov90@gmail.com>
2023-04-28 11:32:03 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -48,6 +48,7 @@ class Battleship:
             ship = Ship(start, end)
             for deck in ship.decks:
                 self.field.update({(deck.row, deck.column): ship})
+        self._validate_field(set(self.field.values()))
 
     def fire(self, location: tuple) -> str:
         if location in self.field.keys():
@@ -75,3 +76,35 @@ class Battleship:
                 else:
                     line += "~    "
             print(f"{line}\n")
+
+    def _validate_field(self, ships: set) -> None:
+        if len(ships) != 10:
+            raise ValueError("The total number of the ships should be 10")
+        ships_counter = {
+            1: ["single-deck", 4, 0],
+            2: ["double-deck", 3, 0],
+            3: ["three-deck", 2, 0],
+            4: ["four-deck", 1, 0]
+        }
+        for ship in ships:
+            ship_decks = len(ship.decks)
+            ships_counter[ship_decks][2] += 1
+        for key in ships_counter:
+            if ships_counter[key][2] != ships_counter[key][1]:
+                if key != 4:
+                    raise ValueError(
+                        f"There should be {ships_counter[key][1]} "
+                        f"{ships_counter[key][0]} ships"
+                    )
+                raise ValueError(f"There should be {ships_counter[key][1]} "
+                                 f"{ships_counter[key][0]} ship")
+
+        keys = self.field.keys()
+        for tested_deck in keys:
+            for other_deck in keys:
+                if (tested_deck != other_deck
+                        and self.field[tested_deck] != self.field[other_deck]):
+                    if (abs(tested_deck[0] - other_deck[0]) < 2
+                            and abs(tested_deck[1] - other_deck[1]) < 2):
+                        raise ValueError("Ships shouldn't be located in the "
+                                         "neighboring cells")
