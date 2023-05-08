b5427598ff072dbe61bd632bd8a8038420b2d5d8
Dmytro Shvets <dirolius@gmail.com>
2023-05-08 14:26:44 +0300

    Solution "is_drowned" added

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from typing import Optional
+
+
 class Deck:
     def __init__(self, row: int, column: int, is_alive: bool = True) -> None:
         self.row = row
@@ -12,25 +15,25 @@ class Ship:
                  is_drowned: bool = False) -> None:
         self.is_drowned = is_drowned
         self.decks = []
-        if start == end:
-            self.decks.append(Deck(*start))
-        elif start[0] == end[0] and start[1] != end[1]:
-            columns = range(start[1], end[1] + 1)
+        for coord_row in range(start[0], end[0] + 1):
+            if start[0] == end[0]:
+                columns = range(start[1], end[1] + 1)
+            else:
+                columns = [start[1]]
             for column in columns:
-                self.decks.append(Deck(start[0], column))
-        else:
-            rows = range(start[0], end[0] + 1)
-            for row in rows:
-                self.decks.append(Deck(row, start[1]))
+                self.decks.append(Deck(coord_row, column))
 
-    def get_deck(self, row: int, column: int) -> Deck:
+    def get_deck(self, row: int, column: int) -> Optional[Deck]:
         for deck in self.decks:
             if deck.row == row and deck.column == column:
                 return deck
 
     def fire(self, row: int, column: int) -> None:
         deck = self.get_deck(row, column)
-        deck.is_alive = False
+        if deck is not None:
+            deck.is_alive = False
+            if all(not deck.is_alive for deck in self.decks):
+                self.is_drowned = True
 
 
 class Battleship:
@@ -40,20 +43,14 @@ class Battleship:
             start, end = coords[0], coords[1]
             ship = Ship(start, end)
             for deck in ship.decks:
-                self.field.update({(deck.row, deck.column): ship})
+                self.field[(deck.row, deck.column)] = ship
 
     def fire(self, location: tuple) -> str:
         if location in self.field:
             ship = self.field[location]
             ship.fire(*location)
 
-            all_ship_decks_destroyed = True
-            for deck in ship.decks:
-                if deck.is_alive:
-                    all_ship_decks_destroyed = False
-                    break
-
-            if all_ship_decks_destroyed:
+            if ship.is_drowned:
                 return "Sunk!"
             return "Hit!"
         return "Miss!"
