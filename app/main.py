1b638b8c93bc5947d6f29e0ed80d55b6d1f1e8db
KirillMelanich <kirillmelanich@gmail.com>
2023-05-30 19:39:17 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,21 +1,16 @@
-from typing import Dict, Tuple, List
-
-
 class Deck:
-    battle_array: List[List[str]] = []
+    battle_array: list[list[str]] = []
 
     def __init__(self, row: int, column: int, is_alive: bool = True) -> None:
         self.row = row
         self.column = column
         self.is_alive = is_alive
-        self.battle_array = [["~" for _ in range(self.row)]
-                             for _ in range(self.column)]
 
 
 class Ship:
     def __init__(self,
-                 start: Tuple[int, int],
-                 end: Tuple[int, int],
+                 start: tuple[int, int],
+                 end: tuple[int, int],
                  is_drowned: bool = False
                  ) -> None:
         self.decks = []
@@ -27,27 +22,36 @@ class Ship:
             for column in range(start[1], end[1] + 1):
                 self.decks.append(Deck(row, column))
 
-    def get_deck(self, row: int, column: int) -> Deck:
+    def get_deck(self, row: int, column: int) -> Deck | None:
         for deck in self.decks:
             if deck.row == row and deck.column == column:
                 return deck
 
     def fire(self, row: int, column: int) -> None:
-        self.get_deck(row, column).is_alive = False
-        self.is_drowned = all(not deck.is_alive for deck in self.decks)
+        deck = self.get_deck(row, column)
+        if deck is not None:
+            deck.is_alive = False
+            for deck in self.decks:
+                self.is_drowned = True
+                if deck.is_alive:
+                    self.is_drowned = False
+        else:
+            raise ValueError(
+                "Invalid location: no deck found at the specified coordinates."
+            )
 
 
 class Battleship:
     def __init__(self,
-                 ships: List[Tuple[Tuple[int, int], Tuple[int, int]]]
+                 ships: list[tuple[tuple[int, int], tuple[int, int]]]
                  ) -> None:
-        self.field: Dict[Tuple[int, int], Ship] = {}
+        self.field: dict[tuple[int, int], Ship] = {}
         for ship in ships:
             battle_ship = Ship(ship[0], ship[1])
             for deck in battle_ship.decks:
                 self.field[(deck.row, deck.column)] = battle_ship
 
-    def fire(self, location: Tuple[int, int]) -> str:
+    def fire(self, location: tuple[int, int]) -> str:
         if location not in self.field:
             return "Miss!"
         ship = self.field[location]
