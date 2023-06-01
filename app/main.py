788291c5943a9a770bb7ecf7eacb03507b984805
admin <admin@Yaroslavs-MacBook-Pro.local>
2023-06-01 12:56:16 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -20,16 +20,13 @@ class Ship:
         self.decks = self.create_decks()
 
     def create_decks(self) -> List[Deck]:
-        decks = []
         if self.start[0] == self.end[0]:
-            for column in range(self.start[1], self.end[1] + 1):
-                deck = Deck(self.start[0], column)
-                decks.append(deck)
+            return ([Deck(self.start[0],
+                          column) for column in
+                     range(self.start[1], self.end[1] + 1)])
         else:
-            for row in range(self.start[0], self.end[0] + 1):
-                deck = Deck(row, self.start[1])
-                decks.append(deck)
-        return decks
+            return ([Deck(row, self.start[1]) for row in
+                     range(self.start[0], self.end[0] + 1)])
 
     def get_deck(self, row: int, column: int) -> Deck:
         for deck in self.decks:
@@ -51,15 +48,15 @@ class Battleship:
     def __init__(self,
                  ships: List[Tuple[Tuple[int, int], Tuple[int, int]]])\
             -> None:
-        self.field = {}
-        for ship in ships:
-            ship_obj = Ship(ship[0], ship[1])
-            for row in range(ship[0][0], ship[1][0] + 1):
-                for column in range(ship[0][1], ship[1][1] + 1):
-                    self.field[(row, column)] = ship_obj
+        ships_obj = [Ship(*ship) for ship in ships]
+        self.field = {
+            (deck.row, deck.column): ship
+            for ship in ships_obj
+            for deck in ship.decks
+        }
 
     def fire(self, location: Tuple[int, int]) -> str:
         if location in self.field:
             ship = self.field[location]
-            return ship.fire(location[0], location[1])
+            return ship.fire(*location)
         return "Miss!"
