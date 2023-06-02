e78f0e532a002d338777414da87b47afd268eec3
Evgen Shepelenko <ShepelenkoEV@gmail.com>
2023-06-02 17:48:32 +0300

    'Solution3'

--- app/main.py
+++ app/main.py
@@ -11,28 +11,24 @@ class Ship:
         self.end = end
         self.is_drowned = False
         self.decks = []
-        if start[0] == end[0] and start[1] == end[1]:
-            self.decks.append(Deck(start[0], end[1]))
-        elif start[0] == end[0]:
-            for column_number in range(start[1], end[1] + 1):
-                self.decks.append(Deck(start[0], column_number))
-        elif start[1] == end[1]:
-            for row_number in range(start[0], end[0] + 1):
-                self.decks.append(Deck(row_number, start[1]))
+        for row in range(start[0], end[0] + 1):
+            for column in range(start[1], end[1] + 1):
+                self.decks.append(Deck(row, column))
 
-    def get_deck(self, row: int, column: int) -> Deck:
+    def get_deck(self, row: int, column: int) -> Deck | None:
         for desk in self.decks:
             if desk.row == row and desk.column == column:
                 return desk
 
     def fire(self, row: int, column: int) -> str:
         fire_desk = self.get_deck(row, column)
+        if fire_desk is None:
+            return "Miss!"
         fire_desk.is_alive = False
-        for desk in self.decks:
-            if desk.is_alive:
-                return "Hit!"
-        self.is_drowned = True
-        return "Sunk!"
+        self.is_drowned = not any(deck.is_alive for deck in self.decks)
+        if self.is_drowned:
+            return "Sunk!"
+        return "Hit!"
 
 
 class Battleship:
@@ -41,7 +37,7 @@ class Battleship:
             for column in range(ship_desk[1] - 1, ship_desk[1] + 2, 2):
                 if ((row, column) in self.field) \
                         and (self.field[(row, column)] != ship):
-                    print("ships shouldn't be located "
+                    raise ValueError("ships shouldn't be located "
                           "in the neighboring cells")
 
     def _validate_field(self) -> None:
@@ -52,21 +48,20 @@ class Battleship:
             self.check_neighbor_cells(ship, ship.start)
             self.check_neighbor_cells(ship, ship.end)
         if deck_ships != {4: 1, 3: 2, 2: 3, 1: 4}:
-            print("the total number of the ships should be 10:\n"
-                  "there should be 4 single-deck ships;\n"
-                  "there should be 3 double-deck ships;\n"
-                  "there should be 2 three-deck ships;\n"
-                  "there should be 1 four-deck ship;")
+            raise ValueError(
+                "the total number of the ships should be 10:\n"
+                "there should be 4 single-deck ships;\n"
+                "there should be 3 double-deck ships;\n"
+                "there should be 2 three-deck ships;\n"
+                "there should be 1 four-deck ship;"
+            )
 
     def __init__(self, ships: list) -> None:
         self.field = {}
         for tuple_ship in ships:
             ship = Ship(tuple_ship[0], tuple_ship[1])
-            if len(ship.decks) == 1:
-                self.field[tuple_ship[0]] = ship
-            else:
-                for deck in ship.decks:
-                    self.field[(deck.row, deck.column)] = ship
+            for deck in ship.decks:
+                self.field[(deck.row, deck.column)] = ship
         self._validate_field()
 
     def fire(self, location: tuple) -> str:
