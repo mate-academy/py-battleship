7c44af925e61b94e62763780224f3af40b89c3af
Oksana Adamchuk <adamchuk.oksana01.08@gmail.com>
2023-06-07 14:22:06 +0300

    refactoring code

--- app/main.py
+++ app/main.py
@@ -11,7 +11,6 @@ class Ship:
             end: "Deck",
             is_drowned: bool = False
     ) -> None:
-        # Create decks and save them to a list `self.decks`
         self.start = start
         self.end = end
         self.is_drowned = is_drowned
@@ -25,16 +24,13 @@ class Ship:
             for i in range(start.row, end.row + 1):
                 self.decks.append(Deck(i, start.column))
 
-    def get_deck(self, row: int, column: int) -> "Deck":
-        # Find the corresponding deck in the list
+    def get_deck(self, row: int, column: int) -> Deck:
         for deck in self.decks:
             if deck.row == row and deck.column == column:
                 return deck
         raise ValueError("There is no such deck")
 
     def fire(self, row: int, column: int) -> None:
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
         deck = self.get_deck(row, column)
         deck.is_alive = not deck.is_alive
         is_decks_alive = []
@@ -48,10 +44,6 @@ class Ship:
 
 class Battleship:
     def __init__(self, ships: list[tuple]) -> None:
-        # Create a dict `self.field`.
-        # Its keys are tuples - the coordinates of the non-empty cells,
-        # A value for each cell is a reference to the ship
-        # which is located in it
         self.field = {}
         for ship in ships:
             new_ship = Ship(
@@ -81,11 +73,7 @@ class Battleship:
             print(" ".join(row))
         return matrix
 
-    def fire(self, location: tuple) -> None:
-        # This function should check whether the location
-        # is a key in the `self.field`
-        # If it is, then it should check if this cell is the last alive
-        # in the ship or not.
+    def fire(self, location: tuple) -> str:
         if location in self.field:
             ship = self.field[location]
             ship.fire(location[0], location[1])
