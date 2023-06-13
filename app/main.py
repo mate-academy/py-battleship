2fb7b7c6182b1028888602a5c7dbd5ad72dac133
Vladimir Titarenko <tytarenkova@gmail.com>
2023-06-13 08:44:45 +0300

    remove unnesserary code in the end of file

--- app/main.py
+++ app/main.py
@@ -147,20 +147,3 @@ class Battleship:
                                     "ships shouldn't be located"
                                     " in the neighboring cells"
                                 )
-
-
-battle_ship = Battleship(
-    ships=[
-        ((0, 0), (0, 3)),
-        ((0, 5), (0, 6)),
-        ((0, 8), (0, 9)),
-        ((2, 0), (4, 0)),
-        ((2, 4), (2, 6)),
-        ((2, 8), (2, 9)),
-        ((9, 9), (9, 9)),
-        ((7, 7), (7, 7)),
-        ((7, 9), (7, 9)),
-        ((9, 7), (9, 7)),
-    ]
-)
-print(battle_ship.print_field())
