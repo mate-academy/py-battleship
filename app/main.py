f43f55883446d06676f8cf638154936f227a90b2
S-SMTN <cven28@gmail.com>
2023-06-16 20:32:41 +0300

    implemented Battleship

--- app/main.py
+++ app/main.py
@@ -55,21 +55,3 @@ class Battleship:
                     return "Sunk!"
                 return "Hit!"
         return "Miss!"
-
-
-if __name__ == '__main__':
-    battle_ship = Battleship(
-        ships=[
-            ((0, 0), (0, 3)),
-            ((0, 5), (0, 6)),
-            ((0, 8), (0, 9)),
-            ((2, 0), (4, 0)),
-            ((2, 4), (2, 6)),
-            ((2, 8), (2, 9)),
-            ((9, 9), (9, 9)),
-            ((7, 7), (7, 7)),
-            ((7, 9), (7, 9)),
-            ((9, 7), (9, 7)),
-        ]
-    )
-    print(battle_ship.fire((0, 4)))
