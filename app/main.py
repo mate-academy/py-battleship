7aa9f87bfe4818c8ae055e2c96119b5afb02344f
Vlad Larin <lordgadyka71@gmail.com>
2023-07-09 17:16:50 +0300

    solution3

--- app/main.py
+++ app/main.py
@@ -147,21 +147,3 @@ def get_surroundings(cell: Tuple[int, int]) -> List[Tuple[int, int]]:
         cell[1] + 1
     )
     return [(i, j) for i in rows for j in columns]
-
-
-battle_ship = Battleship(
-    ships=[
-        ((0, 0), (0, 3)),
-        ((0, 5), (0, 6)),
-        ((0, 8), (0, 9)),
-        ((2, 0), (4, 0)),
-        ((2, 4), (2, 6)),
-        ((2, 8), (2, 9)),
-        ((9, 9), (9, 9)),
-        ((7, 7), (7, 7)),
-        ((7, 9), (7, 9)),
-        ((9, 7), (9, 7)),
-    ]
-)
-
-battle_ship.print_field()
