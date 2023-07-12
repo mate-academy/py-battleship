8b59526d28875d35067f0daed2925d816ab135f3
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-07-12 04:46:39 +0300

    diagonal error

--- app/main.py
+++ app/main.py
@@ -1,13 +1,10 @@
 from typing import Union
 
 from tabulate import tabulate
-
-"""
-~  empty
-□ alive
-* injured
-x drowned
-"""
+import pygame
+import os
+import subprocess
+import platform
 
 
 # deck+deck+deck = Ship
@@ -31,13 +28,15 @@ class Deck:
 class Ship:
     def __init__(self, start: tuple, end: tuple, is_drowned: bool = False
                  ) -> None:  # WHOLE SHIP
+        self.decks: [Deck] = []  # Deck/Decks of every Ship
+
         self.start = start
         self.end = end
 
         self.is_drowned = is_drowned
-        # self.direction = None
 
-        self.decks: [Deck] = []  # Deck/Decks of every Ship
+        self.direction = None
+        self.get_axis()
         self.fill_decks()
 
     def fill_decks(self):
@@ -82,6 +81,17 @@ class Ship:
     # def __repr__(self):
     #     return f"{self.start}, {self.end}, {self.is_drowned}"
 
+    def get_axis(self):  # custom
+        if self.start == self.end:
+            self.direction = "single_point"  # □
+            return
+        if self.start[0] == self.end[0]:
+            self.direction = "x"  # □ □ □
+            return
+        self.direction = "y"  # □
+        # ______________________□
+        # ______________________□
+
 
 class Battleship:
     def __init__(self, ships: list[tuple]):  # Whole game
@@ -98,9 +108,12 @@ class Battleship:
         # self.validate_input()
         # self.print_field()
         # print(self.field)
-        self._validate_input()
+        self.forbidden_cells = []
+        if self._validate_input():
+            self.print_field()
 
     def fire(self, location: tuple):  # Loop is not needed. Just check:
+        print(f"\n\n\nFIRE TO {location} location!")
         # if location in self.field:
         # print(location)
         # deck = Deck(*location)
@@ -115,19 +128,39 @@ class Battleship:
             if point == location:
                 ship.fire(*location)
                 if ship.is_drowned is True:
+
+                    print("Sunk!")
+                    self.print_field()
                     return "Sunk!"
                 else:
+
+                    print("Hit!")
+                    self.print_field()
                     return "Hit!"
+
+        print("Miss!")
+        self.print_field()
         return "Miss!"
         # This function should check whether the location
         # is a key in the `self.field`
         # If it is, then it should check if this cell is the last alive
         # in the ship or not.
 
-    def print_field(self):  # Extra
+    def print_field(self):
 
-        self.field = [["~" for column in range(10)] for field in range(10)]
-        print(tabulate(self.field, tablefmt="grid"))
+        field = [["~" for _ in range(10)] for _ in range(10)]
+
+        for ship in self.field.values():
+            for deck in ship.decks:
+                if deck.is_alive:
+                    field[deck.row][deck.column] = "□"
+                else:
+                    if ship.is_drowned:
+                        field[deck.row][deck.column] = "x"
+                    else:
+                        field[deck.row][deck.column] = "*"
+
+        print(tabulate(field, tablefmt="grid"))
 
     def _validate_input(self):  # Extra
         total_number_of_the_ships = 0
@@ -135,11 +168,31 @@ class Battleship:
         double_deck_counter = 0  # 3
         three_deck_ships = 0  # 2
         four_deck_ships = 0  # 1
-        placement_error = None
+        neighboring_cells = []
+        placement_error = False
 
-        for i in set([ship for ship in self.field.values()]):
+        for ship in set([ship for ship in self.field.values()]):
+
             total_number_of_the_ships += 1
-            unique_ship = i
+            unique_ship = ship
+            for deck in unique_ship.decks:
+                # print(unique_ship.direction)
+                coordinates = (deck.row, deck.column)
+                if coordinates in neighboring_cells:
+                    placement_error = True
+                neighboring_cells.append(coordinates)
+                neighbors = self.get_neighbors(coordinates, unique_ship.direction)
+                neighboring_cells.extend(neighbors)
+
+                # if unique_ship.direction == "y":
+                #     neighboring_cells.append(coordinates[0], coordinates[0])
+                #
+                # if unique_ship.direction == "single_point":
+                #     neighboring_cells.append(coordinates[0], coordinates[0])
+
+                neighboring_cells.append(coordinates)
+            print(neighboring_cells)
+
             if len(unique_ship.decks) == 4:
                 four_deck_ships += 1
             if len(unique_ship.decks) == 3:
@@ -162,9 +215,54 @@ class Battleship:
                 if double_deck_counter == 3:
                     if three_deck_ships == 2:
                         if four_deck_ships == 1:
-                            if placement_error is None:
-                                print(f"{'*' * 10}VALID{'*' * 10}")
+                            if not placement_error:
+                                print(f"\n{'*' * 17} VALID {'*' * 17}\n")
                                 return True
+
+    @staticmethod
+    def get_neighbors(coordinates, direction):
+        row, column = coordinates
+        neighbors = []
+
+        if direction == "x":
+            if column - 1 >= 0:
+                neighbors.append((row, column - 1))
+                if row - 1 >= 0:
+                    neighbors.append((row - 1, column - 1))
+                if row + 1 < 10:
+                    neighbors.append((row + 1, column - 1))
+            if row - 1 >= 0:
+                neighbors.append((row - 1, column))
+            if row + 1 < 10:
+                neighbors.append((row + 1, column))
+            if column + 1 < 10 and row + 1 < 10:
+                neighbors.append((row + 1, column + 1))
+            if column - 1 >= 0 and row + 1 < 10:
+                neighbors.append((row + 1, column - 1))
+
+        if direction == "y":
+            if column - 1 >= 0:
+                neighbors.append((row, column - 1))
+                if row - 1 >= 0:
+                    neighbors.append((row - 1, column - 1))
+            if column + 1 < 10:
+                neighbors.append((row, column + 1))
+                if row - 1 >= 0:
+                    neighbors.append((row - 1, column + 1))
+            if row - 1 >= 0:
+                neighbors.append((row - 1, column))
+            if row + 1 < 10:
+                neighbors.append((row + 1, column))
+
+
+        elif direction == "single_point":
+            for i in range(row - 1, row + 2):
+                for j in range(column - 1, column + 2):
+                    if i == row and j == column:
+                        continue
+                    if 0 <= i < 10 and 0 <= j < 10:
+                        neighbors.append((i, j))
+        return neighbors
 
 
 if __name__ == '__main__':
@@ -183,7 +281,9 @@ if __name__ == '__main__':
             ((9, 7), (9, 7)),
         ]
     )
-    battle_ship.fire((0, 0))
+    # battle_ship.fire((0, 0))
     # battle_ship.fire((0, 1))
     # battle_ship.fire((0, 2))
     # battle_ship.fire((0, 3))
+    # battle_ship.fire((0, 6))
+    # battle_ship.fire((0, 7))
