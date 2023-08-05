a80da1ee50786978eae846266a921601263c4306
Volodymyr Yerokhov <kavbian@gmail.com>
2023-08-05 15:46:31 +0300

    'counting_of_alive_decks'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 class Deck:
-    def __init__(self, row: int, column: int, is_alive: bool = False) -> None:
+    def __init__(self, row: int, column: int, is_alive: bool = True) -> None:
         self.row = row
         self.column = column
+        self.is_alive = is_alive
 
     def __eq__(self, other: tuple) -> bool:
         return self.row == other[0] and self.column == other[1]
@@ -17,12 +18,10 @@ class Ship:
         # Create decks and save them to a list `self.decks`
         self.decks = []
         self.is_drowned = is_drowned
-        self.alive_decks = 0
 
         for row in range(start[0], end[0] + 1):
             for column in range(start[1], end[1] + 1):
                 self.decks.append(Deck(row, column))
-                self.alive_decks += 1
 
     def get_deck(self, row: int, column: int) -> Deck:
         # Find the corresponding deck in the list
@@ -33,8 +32,7 @@ class Ship:
         # Change the `is_alive` status of the deck
         # And update the `is_drowned` value if it's needed
         self.get_deck(row, column).is_alive = False
-        self.alive_decks -= 1
-        if self.alive_decks == 0:
+        if len([deck for deck in self.decks if deck.is_alive]) == 0:
             self.is_drowned = True
 
 
@@ -67,32 +65,3 @@ class Battleship:
                 )
 
         return "Miss!"
-
-
-battle_ship = Battleship(
-    ships=[
-        ((2, 0), (2, 3)),
-        ((4, 5), (4, 6)),
-        ((3, 8), (3, 9)),
-        ((6, 0), (8, 0)),
-        ((6, 4), (6, 6)),
-        ((6, 8), (6, 9)),
-        ((9, 9), (9, 9)),
-        ((9, 5), (9, 5)),
-        ((9, 3), (9, 3)),
-        ((9, 7), (9, 7)),
-    ]
-)
-
-print(battle_ship.fire((0, 4)))
-print(battle_ship.fire((1, 7)))
-print(battle_ship.fire((2, 0)))
-print(battle_ship.fire((2, 1)))
-print(battle_ship.fire((2, 2)))
-print(battle_ship.fire((2, 3)))
-print(battle_ship.fire((4, 3)))
-print(battle_ship.fire((4, 5)))
-print(battle_ship.fire((5, 5)))
-print(battle_ship.fire((4, 6)))
-print(battle_ship.fire((9, 5)))
-print(battle_ship.fire((9, 6)))
