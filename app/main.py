4be7ded6ee0cc51c8820f41a25d191d76cba3ff8
Tanya Burdenko <burdenko.tetyana@gmail.com>
2023-08-24 20:00:28 +0300

    Solution 2

--- app/main.py
+++ app/main.py
@@ -15,23 +15,11 @@ class Ship:
         self.start = start
         self.end = end
         self.is_drowned = is_drowned
-        self.decks = []
-        if start[0] == end[0] and start[1] != end[1]:
-            index = 0
-            for _ in range(start[1], end[1] + 1):
-                self.decks.append(Deck(start[0],
-                                       start[1] + index,
-                                       is_alive=True))
-                index += 1
-        elif start[1] == end[1] and start[0] != end[0]:
-            index = 0
-            for _ in range(start[0], end[0] + 1):
-                self.decks.append(Deck(start[0] + index,
-                                       start[1],
-                                       is_alive=True))
-                index += 1
-        else:
-            self.decks.append(Deck(start[0], start[1], is_alive=True))
+        self.decks = [
+            Deck(row, column)
+            for row in range(start[0], end[0] + 1)
+            for column in range(start[1], end[1] + 1)
+        ]
 
     def get_deck(self, row: int, column: int) -> Deck:
         for deck in self.decks:
@@ -42,30 +30,21 @@ class Ship:
         deck = self.get_deck(row, column)
         if deck:
             deck.is_alive = False
-        check = []
-        for deck in self.decks:
-            if not deck.is_alive:
-                check.append(True)
-            else:
-                check.append(False)
-        if all(check):
-            self.is_drowned = True
+        check = [True if not deck.is_alive else False for deck in self.decks]
+        self.is_drowned = True if all(check) else False
 
 
 class Battleship:
     def __init__(self, ships: list) -> None:
-        # Create a dict `self.field`.
-        # Its keys are tuples - the coordinates of the non-empty cells,
-        # A value for each cell is a reference to the ship
-        # which is located in it
         self.ships = []
         for ship in ships:
             self.ships.append(Ship(ship[0], ship[1], is_drowned=False))
 
-        self.field = {}
-        for ship in self.ships:
-            for deck in ship.decks:
-                self.field[(deck.row, deck.column)] = ship
+        self.field = {
+            (deck.row, deck.column): ship
+            for ship in self.ships
+            for deck in ship.decks
+        }
 
     def fire(self, location: tuple) -> str:
         if location in self.field:
