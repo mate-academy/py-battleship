27c2b037696f7fa72cd069a9ebcf55fe35499f7d
Marina <marishka28023@gmail.com>
2023-09-03 18:58:38 +0200

    'Solution2'

--- app/main.py
+++ app/main.py
@@ -101,25 +101,3 @@ class Battleship:
             table += "\n"
 
         print(table)
-
-
-ships = [
-    ((0, 0), (0, 3)),
-    ((0, 5), (0, 6)),
-    ((0, 8), (0, 9)),
-    ((2, 0), (4, 0)),
-    ((2, 4), (2, 6)),
-    ((2, 8), (2, 9)),
-    ((9, 9), (9, 9)),
-    ((7, 7), (7, 7)),
-    ((7, 9), (7, 9)),
-    ((9, 7), (9, 7)),
-]
-
-battle_ship = Battleship(ships)
-battle_ship.fire((0, 4))
-battle_ship.fire((0, 3))
-battle_ship.fire((0, 2))
-battle_ship.fire((0, 1))
-battle_ship.fire((0, 0))
-battle_ship.print_field()
