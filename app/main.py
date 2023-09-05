017f1e0c4a389ddf865b83e1585d73f11123ef33
Bohdan Kolomiiets <kb907406@gmail.com>
2023-09-05 17:22:06 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,34 +1,121 @@
 class Deck:
-    def __init__(self, row, column, is_alive=True):
-        pass
+    def __init__(self, row: int, column: int, is_alive: bool = True) -> None:
+        self.row = row
+        self.column = column
+        self.is_alive = is_alive
+        self.is_ship_drowned = False
+
+    def __repr__(self) -> str:
+        if self.is_ship_drowned:
+            return "  x  "
+        if not self.is_alive:
+            return "  *  "
+        return u"  \u25A1  "
 
 
 class Ship:
-    def __init__(self, start, end, is_drowned=False):
-        # Create decks and save them to a list `self.decks`
-        pass
+    def __init__(
+            self,
+            start: tuple,
+            end: tuple,
+            is_drowned: bool = False
+    ) -> None:
+        self.decks = []
+        self.is_drowned = is_drowned
+        self.exist = False
+        for row in range(start[0], end[0] + 1):
+            for column in range(start[1], end[1] + 1):
+                self.decks.append(
+                    Deck(
+                        row,
+                        column
+                    )
+                )
 
-    def get_deck(self, row, column):
-        # Find the corresponding deck in the list
-        pass
+    def __repr__(self) -> str:
+        return "  ".join([str(deck) for deck in self.decks])
 
-    def fire(self, row, column):
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
-        pass
+    def change_repr(self) -> None:
+        for deck in self.decks:
+            deck.is_ship_drowned = True
+
+    def get_deck(self, row: int, column: int) -> Deck | str:
+        for deck in self.decks:
+            if deck.row == row and deck.column == column:
+                return deck
+        # return f"There is not deck of this ship in ({row}, {column}) cell"
+
+    def fire(self, row: int, column: int) -> None:
+        alive_list = []
+
+        for deck in self.decks:
+            if deck.row == row and deck.column == column:
+                deck.is_alive = False
+            alive_list.append(deck.is_alive)
+        if not any(alive_list):
+            self.is_drowned = True
+            self.change_repr()
 
 
 class Battleship:
-    def __init__(self, ships):
-        # Create a dict `self.field`.
-        # Its keys are tuples - the coordinates of the non-empty cells,
-        # A value for each cell is a reference to the ship
-        # which is located in it
-        pass
+    def __init__(self, ships: list[tuple]) -> None:
+        self.field = {}
+        self.warships = []
+        for ship in ships:
 
-    def fire(self, location: tuple):
-        # This function should check whether the location
-        # is a key in the `self.field`
-        # If it is, then it should check if this cell is the last alive
-        # in the ship or not.
-        pass
+            start = ship[0]
+            end = ship[1]
+            warship = Ship(start, end)
+            self.warships.append(warship)
+
+            for row in range(start[0], end[0] + 1):
+                for column in range(start[1], end[1] + 1):
+                    self.field[(row, column)] = warship
+
+    def fire(self, location: tuple) -> str:
+        if location in self.field:
+            self.field[location].fire(location[0], location[1])
+            if self.field[location].is_drowned:
+                return "Sunk!"
+            return "Hit!"
+        return "Miss!"
+
+    def print_field(self) -> None:
+        field = [["  ~  " for _ in range(10)] for _ in range(10)]
+
+        for ship in self.field.values():
+            if not ship.exist:
+                for deck in ship.decks:
+                    field[deck.row][deck.column] = str(deck)
+
+        for row in field:
+            print("".join(row))
+
+    # def _validate_field(self) -> str:
+    #     if len(self.warships) != 10:
+    #         return "The total number of the ships should be 10"
+    #
+    #     count_single_decks_ship = 0
+    #     count_double_decks_ship = 0
+    #     count_triple_deck_ship = 0
+    #     count_four_deck_ship = 0
+    #
+    #     for ship in self.warships:
+    #
+    #         if len(ship.decks) == 1:
+    #             count_single_decks_ship += 1
+    #         if len(ship.decks) == 2:
+    #             count_double_decks_ship += 1
+    #         if len(ship.decks) == 3:
+    #             count_triple_deck_ship += 1
+    #         if len(ship.decks) == 4:
+    #             count_four_deck_ship += 1
+    #
+    #     if count_single_decks_ship != 4:
+    #         return "There should be 4 single-deck ships"
+    #     if count_double_decks_ship != 3:
+    #         return "There should be 3 double-deck ships"
+    #     if count_triple_deck_ship != 2:
+    #         return "There should be 2 three-deck ships"
+    #     if count_four_deck_ship != 1:
+    #         return "There should be 1 four-deck ships"
