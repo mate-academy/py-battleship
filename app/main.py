0483c62e011a10431438556a2ae58e0ae2b39dc2
3lobites <3lobites@gmail.com>
2023-09-11 16:07:52 +0300

    work_20230911

--- app/main.py
+++ app/main.py
@@ -1,12 +1,37 @@
+from typing import List, Tuple
+
+
 class Deck:
     def __init__(self, row, column, is_alive=True):
-        pass
+        self.row = row
+        self.column = column
+        self.is_alive = is_alive
 
 
 class Ship:
-    def __init__(self, start, end, is_drowned=False):
-        # Create decks and save them to a list `self.decks`
-        pass
+
+    def __init__(self, start: tuple, end: tuple, is_drowned=False):
+        self.start = start
+        self.end = end
+        self.is_drowned = is_drowned
+        self.ship_deck = self.init_ship_decks()
+
+    def init_ship_decks(self) -> List[Deck]:
+        list_decks = []
+
+        if self.start[0] == self.start[1] and self.end[0] == self.end[1]:
+            list_decks.append(Deck(self.start[0], self.start[1]))
+        else:
+            if self.start[0] == self.end[0]:
+                # for x
+                for work_y in range(self.start[1], self.end[1] + 1):
+                    list_decks.append(Deck(self.start[0], work_y))
+            if self.start[1] == self.end[1]:
+                # for y
+                for work_x in range(self.start[0], self.end[0] + 1):
+                    list_decks.append(Deck(work_x, self.start[1]))
+
+        return list_decks
 
     def get_deck(self, row, column):
         # Find the corresponding deck in the list
@@ -19,16 +44,60 @@ class Ship:
 
 
 class Battleship:
-    def __init__(self, ships):
+
+    Ships_in_battle = []
+    ship_all_place = []
+
+    def __init__(self, ships: List[Tuple]):
         # Create a dict `self.field`.
         # Its keys are tuples - the coordinates of the non-empty cells,
         # A value for each cell is a reference to the ship
         # which is located in it
-        pass
+        self.ships = ships
+        self.init_ships_in_battle()
+        self.init_all_place_ship()
+
+    def init_ships_in_battle(self) -> None:
+        for work_ship in self.ships:
+            self.Ships_in_battle.append(Ship(work_ship[0], work_ship[1]))
+
+    def init_all_place_ship(self) -> None:
+        for data_ship in self.ships:
+            if data_ship[0][0] == data_ship[1][0] and data_ship[0][1] == data_ship[1][1]:
+                self.ship_all_place.append((data_ship[0][0], data_ship[0][1]))
+            else:
+                if data_ship[0][0] == data_ship[1][0]:
+                    # for x
+                    for work_y in range(data_ship[1][0], data_ship[1][1] + 1):
+                        self.ship_all_place.append((data_ship[0][0], work_y))
+                if data_ship[0][1] == data_ship[1][1]:
+                    # for y
+                    for work_x in range(data_ship[0][1], data_ship[1][1] + 1):
+                        self.ship_all_place.append((work_x, data_ship[1][1]))
 
     def fire(self, location: tuple):
         # This function should check whether the location
         # is a key in the `self.field`
         # If it is, then it should check if this cell is the last alive
         # in the ship or not.
-        pass
+        if location in self.Ships_in_battle:
+            pass
+
+
+battle_ship = Battleship(
+    ships=[
+        ((0, 0), (0, 3)),
+        ((0, 5), (0, 6)),
+        ((0, 8), (0, 9)),
+        ((2, 0), (4, 0)),
+        ((2, 4), (2, 6)),
+        ((2, 8), (2, 9)),
+        ((9, 9), (9, 9)),
+        ((7, 7), (7, 7)),
+        ((7, 9), (7, 9)),
+        ((9, 7), (9, 7)),
+    ]
+    # [((2, 8), (2, 9)), ((2, 4), (2, 6))]
+)
+
+ttt = 0
