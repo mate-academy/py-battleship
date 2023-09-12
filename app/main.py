8fa718cdeca67e1f03b46bdf60179d7911a00545
3lobites <3lobites@gmail.com>
2023-09-12 10:37:02 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from typing import List, Tuple
 
 
 class Deck:
-    def __init__(self, row, column, is_alive=True):
+    def __init__(self, row: int, column: int, is_alive: bool = True) -> None:
         self.row = row
         self.column = column
         self.is_alive = is_alive
@@ -10,7 +10,12 @@ class Deck:
 
 class Ship:
 
-    def __init__(self, start: tuple, end: tuple, is_drowned=False):
+    def __init__(
+            self,
+            start: tuple,
+            end: tuple,
+            is_drowned: bool = False
+    ) -> None:
         self.start = start
         self.end = end
         self.is_drowned = is_drowned
@@ -33,14 +38,26 @@ class Ship:
 
         return list_decks
 
-    def get_deck(self, row, column):
+    def get_deck(self, row: int, column: int) -> bool:
         # Find the corresponding deck in the list
-        pass
+        for target_deck in self.ship_deck:
+            if (row, column) == (target_deck.row, target_deck.column):
+                return target_deck.is_alive
 
-    def fire(self, row, column):
+    def alive_or_dead_ship(self) -> bool:
+        ship_info_res = []
+        for ship_info in self.ship_deck:
+            ship_info_res.append(ship_info.is_alive)
+        alive_or_dead = any(ship_info_res)
+        return alive_or_dead
+
+    def fire(self, row: int, column: int) -> None:
         # Change the `is_alive` status of the deck
         # And update the `is_drowned` value if it's needed
-        pass
+        for target_deck in self.ship_deck:
+            if (row, column) == (target_deck.row, target_deck.column):
+                if target_deck.is_alive:
+                    target_deck.is_alive = False
 
 
 class Battleship:
@@ -48,7 +65,7 @@ class Battleship:
     Ships_in_battle = []
     ship_all_place = []
 
-    def __init__(self, ships: List[Tuple]):
+    def __init__(self, ships: List[Tuple]) -> None:
         # Create a dict `self.field`.
         # Its keys are tuples - the coordinates of the non-empty cells,
         # A value for each cell is a reference to the ship
@@ -63,7 +80,10 @@ class Battleship:
 
     def init_all_place_ship(self) -> None:
         for data_ship in self.ships:
-            if data_ship[0][0] == data_ship[1][0] and data_ship[0][1] == data_ship[1][1]:
+            if (
+                    data_ship[0][0] == data_ship[1][0]
+                    and data_ship[0][1] == data_ship[1][1]
+            ):
                 self.ship_all_place.append((data_ship[0][0], data_ship[0][1]))
             else:
                 if data_ship[0][0] == data_ship[1][0]:
@@ -80,47 +100,13 @@ class Battleship:
         # is a key in the `self.field`
         # If it is, then it should check if this cell is the last alive
         # in the ship or not.
-        ooo = 0
 
         if location not in self.ship_all_place:
             return "Miss!"
         for target_ship in self.Ships_in_battle:
-            # тут обработать палубы при помощи функции get_deck которая вернет статус палуб и статус самого корабля
-            for target_deck in target_ship.ship_deck:
-                yyyy = (target_deck.row, target_deck.column)
-                if location == (target_deck.row, target_deck.column):
-                    if target_deck.is_alive:
-                        # тут обработку подбитой палубы
-                        return "Hit!"
-                    else:
-                        return "Miss!"
-
-
-
-
-
-battle_ship = Battleship(
-    ships=[
-        ((0, 0), (0, 3)),
-        ((0, 5), (0, 6)),
-        ((0, 8), (0, 9)),
-        ((2, 0), (4, 0)),
-        ((2, 4), (2, 6)),
-        ((2, 8), (2, 9)),
-        ((9, 9), (9, 9)),
-        ((7, 7), (7, 7)),
-        ((7, 9), (7, 9)),
-        ((9, 7), (9, 7)),
-    ]
-    # [((2, 8), (2, 9)), ((2, 0), (4, 0))]
-)
-
-print(
-    battle_ship.fire((0, 4)),  # Miss!
-    battle_ship.fire((0, 3)),  # Hit!
-    battle_ship.fire((0, 2)),  # Hit!
-    battle_ship.fire((0, 1)),  # Hit!
-    battle_ship.fire((0, 0)),  # Sunk!
-)
-
-ttt = 0
+            if target_ship.get_deck(location[0], location[1]):
+                target_ship.fire(location[0], location[1])
+                if target_ship.alive_or_dead_ship():
+                    return "Hit!"
+                else:
+                    return "Sunk!"
