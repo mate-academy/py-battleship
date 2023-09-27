63f340879f0d902e342d799a9497673bf0121a20
vladyslav shepilov <shepsterya@gmail.com>
2023-09-27 16:34:28 +0300

    refactored and removed unnecessary if condition

--- app/main.py
+++ app/main.py
@@ -13,20 +13,19 @@ class Ship:
     def __init__(self, start: tuple, end: tuple,
                  is_drowned: bool = False) -> None:
         self.is_drowned = is_drowned
-        self.coordinates = tuple([Deck(x, y)
-                                  for x in range(start[0], end[0] + 1)
-                                  for y in range(start[1], end[1] + 1)])
+        self.decks = [Deck(x, y)
+                      for x in range(start[0], end[0] + 1)
+                      for y in range(start[1], end[1] + 1)]
 
     def get_deck(self, row: int, column: int) -> Deck:
-        for deck in self.coordinates:
+        for deck in self.decks:
             if deck.get_coords() == (row, column):
                 return deck
         raise ValueError
 
     def fire(self, row: int, column: int) -> None:
         self.get_deck(row, column).is_alive = False
-        if all([not deck.is_alive for deck in self.coordinates]):
-            self.is_drowned = True
+        self.is_drowned = all([not deck.is_alive for deck in self.decks])
 
 
 class Battleship:
@@ -34,7 +33,7 @@ class Battleship:
         self.field = {}
         for ship in ships:
             new_ship = Ship(*ship)
-            for coords in new_ship.coordinates:
+            for coords in new_ship.decks:
                 self.field[coords.get_coords()] = new_ship
 
     def fire(self, location: tuple) -> str:
