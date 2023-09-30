a389301a869b4817a9c82ca51bba43677f4c0193
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2023-09-30 21:24:26 +0100

    battle-ship

--- app/main.py
+++ app/main.py
@@ -20,15 +20,78 @@ class Ship:
 
 class Battleship:
     def __init__(self, ships):
-        # Create a dict `self.field`.
-        # Its keys are tuples - the coordinates of the non-empty cells,
-        # A value for each cell is a reference to the ship
-        # which is located in it
-        pass
+        matrix = [["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                  ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                  ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                  ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                  ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                  ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                  ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                  ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                  ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
+                  ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"]]
+        for ship in ships:
+            if ship[0] == ship[1]:
+                matrix[ship[0][0]][ship[0][1]] = "□"
+            if ship[0][0] != ship[1][0]:
+                for length_shipy in range(ship[0][0], ship[1][0] + 1):
+                    matrix[length_shipy][ship[1][1]] = "□"
+            if ship[0][1] != ship[1][1]:
+                for length_shipx in range(ship[0][1], ship[1][1] + 1):
+                    matrix[ship[0][0]][length_shipx] = "□"
+        self.matrix = matrix
+        self.ships = ships
 
     def fire(self, location: tuple):
-        # This function should check whether the location
-        # is a key in the `self.field`
-        # If it is, then it should check if this cell is the last alive
-        # in the ship or not.
-        pass
+        y = location[0]
+        x = location[1]
+        if y == 9 and x == 9:
+            if self.matrix[y][x] == "□" and self.matrix[y][x - 1] != "□" and self.matrix[y - 1][x] != "□":
+                self.matrix[y][x] = "X"
+                return "Sunk!"
+        elif y == 0 and x == 0:
+            if self.matrix[y][x] == "□" and self.matrix[y][x + 1] != "□" and self.matrix[y + 1][x] != "□":
+                self.matrix[y][x] = "X"
+                return "Sunk!"
+        elif y == 9 and x == 0:
+            if self.matrix[y][x] == "□" and self.matrix[y][x + 1] != "□" and self.matrix[y - 1][x] != "□":
+                self.matrix[y][x] = "X"
+                return "Sunk!"
+        elif y == 0 and x == 9:
+            if self.matrix[y][x] == "□" and self.matrix[y][x - 1] != "□" and self.matrix[y + 1][x] != "□":
+                self.matrix[y][x] = "X"
+                return "Sunk!"
+        elif y == 9:
+            if self.matrix[y][x] == "□" and self.matrix[y][x + 1] != "□" and self.matrix[y - 1][x] != "□" and self.matrix[y][x - 1] != "□":
+                self.matrix[y][x] = "X"
+                return "Sunk!"
+        elif x == 9:
+            if self.matrix[y][x] == "□" and self.matrix[y][x - 1] != "□" and self.matrix[y - 1][x] != "□" and self.matrix[y + 1][x] != "□":
+                self.matrix[y][x] = "X"
+                return "Sunk!"
+        elif self.matrix[y][x] == "□" and self.matrix[y][x + 1] != "□" and self.matrix[y][x - 1] != "□" and self.matrix[y + 1][x] != "□" and self.matrix[y - 1][x] != "□":
+            self.matrix[y][x] = "X"
+            return "Sunk!"
+        elif self.matrix[y][x] == "□":
+            self.matrix[y][x] = "*"
+            return "Hit!"
+        self.matrix[y][x] = "X"
+        return "Miss!"
+    def __repr__(self):
+        result = ""
+        for line in self.matrix:
+            result += "  ".join(line) + "\n"
+        return result
+    def _validate_field(self):
+        real ={4: 0, 3: 0, 2: 0, 1: 0}
+        must_be = {4: 1, 3: 2, 2: 3, 1: 4}
+        if len(self.ships) != 10:
+            return False
+        for ship in self.ships:
+            if ship[0] == ship[1]:
+                real[1] += 1
+            if ship[0][0] != ship[1][0]:
+                real[ship[1][0] - ship[0][0] + 1] += 1
+            if ship[0][1] != ship[1][1]:
+                real[ship[1][1] - ship[0][1] + 1] += 1
+        return real == must_be
