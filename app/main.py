018da5519e6615dad12bf5a6301b49f089266719
Stepan Shevchuk <stepka.shevchuk@gmail.com>
2023-10-01 18:18:21 +0100

    battle-ship

--- app/main.py
+++ app/main.py
@@ -1,7 +1,44 @@
+class Deck:
+    def __init__(self, row, column, is_alive=True):
+        self.row = row
+        self.column = column
+        self.is_alive = is_alive
 
+
+class Ship:
+    def __init__(self, start, end, is_drowned=False):
+        self.ship = []
+        if start == end:
+            self.ship.extend([Deck(start[0], start[1])])
+        elif start[1] != end[1]:
+            self.ship.extend([Deck(start[0], deck) for deck in range(start[1], end[1] + 1)])
+        elif start[0] != end[0]:
+            self.ship.extend([Deck(deck, start[1]) for deck in range(start[0], end[0] + 1)])
+        self.is_drowned = is_drowned
+
+    def fire(self, row, column):
+        for deck in self.ship:
+            if deck.row == row and deck.column == column:
+                deck.is_alive = False
+                self.ship.remove(deck)
+        if len(self.ship) == 0:
+            self.is_drowned = True
+            return "Sunk!"
+        return "Hit!"
 
 class Battleship:
     def __init__(self, ships: list[tuple]) -> None:
+        self.ships = [Ship(ship[0], ship[1]) for ship in ships]
+
+    def fire(self, location: tuple) -> str:
+        for ship in self.ships:
+            for deck in ship.ship:
+                if deck.row == location[0] and deck.column == location[1]:
+                    return ship.fire(location[0], location[1])
+        return "Miss!"
+
+    def __repr__(self) -> str:
+
         matrix = [["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
                   ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
                   ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
@@ -12,88 +49,20 @@ class Battleship:
                   ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
                   ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"],
                   ["~", "~", "~", "~", "~", "~", "~", "~", "~", "~"]]
-        for ship in ships:
-            if ship[0] == ship[1]:
-                matrix[ship[0][0]][ship[0][1]] = "□"
-            if ship[0][0] != ship[1][0]:
-                for length_shipy in range(ship[0][0], ship[1][0] + 1):
-                    matrix[length_shipy][ship[1][1]] = "□"
-            if ship[0][1] != ship[1][1]:
-                for length_shipx in range(ship[0][1], ship[1][1] + 1):
-                    matrix[ship[0][0]][length_shipx] = "□"
-        self.matrix = matrix
-        self.ships = ships
-
-    def fire(self, location: tuple) -> str:
-        y_fire = location[0]
-        x_fire = location[1]
-        if y_fire == 9 and x_fire == 9:
-            if (self.matrix[y_fire][x_fire] == "□"
-                    and self.matrix[y_fire][x_fire - 1] != "□"
-                    and self.matrix[y_fire - 1][x_fire] != "□"):
-                self.matrix[y_fire][x_fire] = "X"
-                return "Sunk!"
-        elif y_fire == 0 and x_fire == 0:
-            if (self.matrix[y_fire][x_fire] == "□"
-                    and self.matrix[y_fire][x_fire + 1] != "□"
-                    and self.matrix[y_fire + 1][x_fire] != "□"):
-                self.matrix[y_fire][x_fire] = "X"
-                return "Sunk!"
-        elif y_fire == 9 and x_fire == 0:
-            if (self.matrix[y_fire][x_fire] == "□"
-                    and self.matrix[y_fire][x_fire + 1] != "□"
-                    and self.matrix[y_fire - 1][x_fire] != "□"):
-                self.matrix[y_fire][x_fire] = "X"
-                return "Sunk!"
-        elif y_fire == 0 and x_fire == 9:
-            if (self.matrix[y_fire][x_fire] == "□"
-                    and self.matrix[y_fire][x_fire - 1] != "□"
-                    and self.matrix[y_fire + 1][x_fire] != "□"):
-                self.matrix[y_fire][x_fire] = "X"
-                return "Sunk!"
-        elif y_fire == 9:
-            if (self.matrix[y_fire][x_fire] == "□"
-                    and self.matrix[y_fire][x_fire + 1] != "□"
-                    and self.matrix[y_fire - 1][x_fire] != "□"
-                    and self.matrix[y_fire][x_fire - 1] != "□"):
-                self.matrix[y_fire][x_fire] = "X"
-                return "Sunk!"
-        elif x_fire == 9:
-            if (self.matrix[y_fire][x_fire] == "□"
-                    and self.matrix[y_fire][x_fire - 1] != "□"
-                    and self.matrix[y_fire - 1][x_fire] != "□"
-                    and self.matrix[y_fire + 1][x_fire] != "□"):
-                self.matrix[y_fire][x_fire] = "X"
-                return "Sunk!"
-        elif (self.matrix[y_fire][x_fire] == "□"
-              and self.matrix[y_fire][x_fire + 1] != "□"
-              and self.matrix[y_fire][x_fire - 1] != "□"
-              and self.matrix[y_fire + 1][x_fire] != "□"
-              and self.matrix[y_fire - 1][x_fire] != "□"):
-            self.matrix[y_fire][x_fire] = "X"
-            return "Sunk!"
-        elif self.matrix[y_fire][x_fire] == "□":
-            self.matrix[y_fire][x_fire] = "*"
-            return "Hit!"
-        self.matrix[y_fire][x_fire] = "X"
-        return "Miss!"
-
-    def __repr__(self) -> str:
+        for ship in self.ships:
+            for deck in ship.ship:
+                matrix[deck.row][deck.column] = "□"
         result = ""
-        for line in self.matrix:
-            result += "  ".join(line) + "\n"
+        for row in matrix:
+            result += "  ".join(row) + "\n"
         return result
 
-    def _validate_field(self) -> bool:
+    def validate_field(self) -> bool:
         real = {4: 0, 3: 0, 2: 0, 1: 0}
         must_be = {4: 1, 3: 2, 2: 3, 1: 4}
         if len(self.ships) != 10:
             return False
         for ship in self.ships:
-            if ship[0] == ship[1]:
-                real[1] += 1
-            if ship[0][0] != ship[1][0]:
-                real[ship[1][0] - ship[0][0] + 1] += 1
-            if ship[0][1] != ship[1][1]:
-                real[ship[1][1] - ship[0][1] + 1] += 1
+            print(len(ship.ship))
+            real[len(ship.ship)] += 1
         return real == must_be
