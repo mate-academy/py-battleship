3ffb770a9a886791c6d7c774e778e512f1445633
Kristina <tina.vasylenko@gmail.com>
2023-10-09 17:17:56 +0300

    Solution1

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ class Ship:
             for row in range(start[0], end[1] + 1):
                 self.decks.append(Deck(row, column))
 
-    def get_deck(self, row: int, column: int) -> Deck:
+    def get_deck(self, row: int, column: int) -> Deck | None:
         for deck in self.decks:
             if deck.row == row and deck.column == column:
                 return deck
@@ -42,7 +42,7 @@ class Ship:
         deck = self.get_deck(row, column)
         if deck:
             deck.is_alive = False
-            self.is_drowned = not any(d.is_alive for d in self.decks)
+            self.is_drowned = not any(deck.is_alive for deck in self.decks)
 
 
 class Battleship:
