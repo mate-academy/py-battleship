9594016223c0cbd479e23bad13805f21225ea26e
naz-iv <nazarivankiv1@gmail.com>
2023-10-24 20:04:18 +0300

    Work in progress

--- app/main.py
+++ app/main.py
@@ -1,26 +1,52 @@
+from ast import List
+import random
+
+
 class Deck:
     def __init__(self, row, column, is_alive=True):
-        pass
+        self.row = row
+        self.column = column
+        self.is_alive = is_alive
+
+    def get_deck(self) -> tuple:
+        return (self.row, self.column)
 
 
 class Ship:
-    def __init__(self, start, end, is_drowned=False):
-        # Create decks and save them to a list `self.decks`
-        pass
 
-    def get_deck(self, row, column):
-        # Find the corresponding deck in the list
-        pass
+    def __init__(self, start: tuple, end: tuple, is_drowned=False):
+        self.decks = self._decks_generator(start, end)
+        self.is_drowned = is_drowned
 
-    def fire(self, row, column):
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
-        pass
+    def get_deck(self, row: tuple, column: tuple) -> Deck:
+        for deck in self.decks:
+            if deck.row == row and deck.column == column:
+                return deck
+
+    def fire(self, row: tuple, column: tuple):
+        deck = self.get_deck(row, column)
+        if deck:
+            deck.is_alive = False
+            decks_alive = list(filter(lambda x: x.is_alive == True, self.decks))
+            if not decks_alive:
+                self.is_drowned = True
+                return "Sunk!"
+            return "Hit!"
+        return "Miss!"
+
+    @staticmethod
+    def _decks_generator(start: tuple, end: tuple) -> list:
+        start_row, end_row = start
+        start_column, end_column = end
+        if start_row == end_row:
+            return [Deck(start_column, cell) for cell in range(start_column, end_column + 1)]
+        return [Deck(start_row, row) for row in range(start_row, end_row + 1)]
 
 
 class Battleship:
     def __init__(self, ships):
         # Create a dict `self.field`.
+        self.fields = get_fields(ships)
         # Its keys are tuples - the coordinates of the non-empty cells,
         # A value for each cell is a reference to the ship
         # which is located in it
@@ -32,3 +58,5 @@ class Battleship:
         # If it is, then it should check if this cell is the last alive
         # in the ship or not.
         pass
+
+    def get_fields(self, ships) -> dict:
