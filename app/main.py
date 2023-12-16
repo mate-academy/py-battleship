8176cd48b84c6d08766a2ba005742e142c2e3f0b
Nataliia Povashevych <interchanal@ukr.net>
2023-12-16 16:49:10 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ class Ship:
             for column in range(start[1], end[1] + 1):
                 self.decks.append(Deck(row, column))
 
-    def get_deck(self, row: int, column: int) -> Deck:
+    def get_deck(self, row: int, column: int) -> Deck | None:
         for deck in self.decks:
             if deck.row == row and deck.column == column:
                 return deck
