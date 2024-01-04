bdb3fa459b8f0e26ee3dfaec5fadbe88b81cff25
Vlad <mr.darmstadtium@gmail.com>
2024-01-04 19:52:16 +0100

    wrote code before battleship

--- app/main.py
+++ app/main.py
@@ -1,21 +1,37 @@
 class Deck:
-    def __init__(self, row, column, is_alive=True):
-        pass
+    def __init__(self, row, column, is_alive=True) -> None:
+        self.field = (row, column)
+        self.is_alive = is_alive
+
+    def change_is_alive(self):
+        if self.is_alive:
+            self.is_alive = False
 
 
 class Ship:
-    def __init__(self, start, end, is_drowned=False):
+    def __init__(self, start, end, is_drowned=False) -> None:
         # Create decks and save them to a list `self.decks`
-        pass
+        self.decks = []
+        self.decks.append(Deck(start, end))
+        self.is_drowned = is_drowned
 
-    def get_deck(self, row, column):
+    def get_deck(self, row, column) -> Deck | bool:
         # Find the corresponding deck in the list
-        pass
+        for deck in self.decks:
+            if deck.field == (row, column):
+                return deck
+        return False
 
-    def fire(self, row, column):
+    def fire(self, row, column) -> None:
         # Change the `is_alive` status of the deck
         # And update the `is_drowned` value if it's needed
-        pass
+        deck = self.get_deck(row, column)
+
+        if deck:
+            deck.change_is_alive()
+
+        if all(not deck.is_alive for deck in self.decks):
+            self.is_drowned = True
 
 
 class Battleship:
