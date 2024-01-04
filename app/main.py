4245cf3ca98575d7a3c3bc17e05dbb8f3d482765
Maksym <tarasyukmaxim@gmail.com>
2024-01-04 11:03:18 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -19,35 +19,35 @@ class Ship:
             is_drowned: bool = False
     ) -> None:
 
-        self.start = Deck(start[0], start[1])
-        self.end = Deck(end[0], end[1])
+        self.start = Deck(*start)
+        self.end = Deck(*end)
         self.is_drowned = is_drowned
         self.decks = []
         self.create_decks()
 
     def create_decks(self) -> None:
-        intermediate_coordinates = []
+        deck_coordinates = []
 
         if self.start.row == self.end.row:
-            intermediate_coordinates = [
+            deck_coordinates = [
                 (self.start.row, col) for col in range(
                     self.start.column, self.end.column + 1)]
 
         elif self.start.column == self.end.column:
-            intermediate_coordinates = [
+            deck_coordinates = [
                 (row, self.start.column) for row in range(
                     self.start.row, self.end.row + 1)]
 
-        for coord in intermediate_coordinates:
+        for coord in deck_coordinates:
             decks_instance = Deck(coord[0], coord[1])
             self.decks.append(decks_instance)
 
-    def get_deck(self, row: tuple, column: tuple) -> Deck:
+    def get_deck(self, row: int, column: int) -> Deck:
         for deck in self.decks:
             if deck.row == row and deck.column == column:
                 return deck
 
-    def fire(self, row: tuple, column: tuple) -> None:
+    def fire(self, row: int, column: int) -> None:
         hit_deck = self.get_deck(row, column)
 
         if hit_deck and hit_deck.is_alive:
@@ -57,7 +57,7 @@ class Ship:
                 self.is_drowned = True
 
 
-class Battleship:
+class Battleship():
     def __init__(self, ships: list[tuple[tuple[int]]]) -> None:
         self.ships = ships
         self.field = {}
@@ -71,19 +71,12 @@ class Battleship:
                 self.field[deck.row, deck.column] = ship
 
     def fire(self, location: tuple) -> str:
+        row, column = location
+        fired_ship = self.field.get((row, column))
 
-        if location in self.field.keys():
-            fired_ship = self.field.get(location)
-
-            hit_deck = fired_ship.get_deck(location[0], location[1])
-
-            if hit_deck and hit_deck.is_alive:
-                hit_deck.is_alive = False
-
-                if all(not deck.is_alive for deck in fired_ship.decks):
-                    fired_ship.is_drowned = True
-                    return "Sunk!"
-                else:
-                    return "Hit!"
-        else:
-            return "Miss!"
+        if fired_ship:
+            fired_ship.fire(row, column)
+            if fired_ship.is_drowned:
+                return "Sunk!"
+            return "Hit!"
+        return "Miss!"
