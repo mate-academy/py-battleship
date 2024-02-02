9eb973682e17014c77950b2560e3772b4dda73eb
Andrii Zubchenko <dissom1987@gmail.com>
2024-02-02 21:30:02 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -75,32 +75,3 @@ class Battleship:
                 else:
                     print("~", end="\t")
             print()
-
-
-if __name__ == "__main__":
-    battle_ship = Battleship(
-        ships=[
-            ((0, 0), (0, 3)),
-            ((0, 5), (0, 6)),
-            ((0, 8), (0, 9)),
-            ((2, 0), (4, 0)),
-            ((2, 4), (2, 6)),
-            ((2, 8), (2, 9)),
-            ((9, 9), (9, 9)),
-            ((7, 7), (7, 7)),
-            ((7, 9), (7, 9)),
-            ((9, 7), (9, 7)),
-        ]
-    )
-
-    printer = battle_ship
-
-    printer.print_field()
-
-    print(
-        battle_ship.fire((0, 4)),  # Miss!
-        battle_ship.fire((0, 3)),  # Hit!
-        battle_ship.fire((0, 2)),  # Hit!
-        battle_ship.fire((0, 1)),  # Hit!
-        battle_ship.fire((0, 0)),  # Sunk!
-    )
