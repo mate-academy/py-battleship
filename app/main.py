4e117ffdcbfac91c2acac03133524721ae35c29b
MaxymChyncha <max.chyncha@gmail.com>
2024-02-04 17:07:22 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,34 +1,104 @@
 class Deck:
-    def __init__(self, row, column, is_alive=True):
-        pass
+    def __init__(
+            self,
+            row: int,
+            column: int,
+            is_alive: bool = True
+    ) -> None:
+        self.row = row
+        self.column = column
+        self.is_alive = is_alive
 
 
 class Ship:
-    def __init__(self, start, end, is_drowned=False):
-        # Create decks and save them to a list `self.decks`
-        pass
+    def __init__(
+            self,
+            start: tuple[int, int],
+            end: tuple[int, int],
+            is_drowned: bool = False
+    ) -> None:
+        self.decks = self._create_ship_decks(start, end)
+        self.is_drowned = is_drowned
 
-    def get_deck(self, row, column):
-        # Find the corresponding deck in the list
-        pass
+    @staticmethod
+    def _create_ship_decks(
+            start: tuple[int, int],
+            end: tuple[int, int]
+    ) -> list[Deck]:
+        return [
+            Deck(row=row, column=column)
+            for row in range(start[0], end[0] + 1)
+            for column in range(start[1], end[1] + 1)
+        ]
 
-    def fire(self, row, column):
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
-        pass
+    def get_deck(self, row: int, column: int) -> Deck:
+        for deck in self.decks:
+            if deck.row == row and deck.column == column:
+                return deck
+
+    def fire(self, row: int, column: int) -> None:
+        deck = self.get_deck(row, column)
+        deck.is_alive = False
+        self.is_drowned = not any(deck.is_alive for deck in self.decks)
 
 
 class Battleship:
-    def __init__(self, ships):
-        # Create a dict `self.field`.
-        # Its keys are tuples - the coordinates of the non-empty cells,
-        # A value for each cell is a reference to the ship
-        # which is located in it
-        pass
+    def __init__(
+            self,
+            ships: list[tuple[tuple[int, int], tuple[int, int]]]
+    ) -> None:
+        self.printed_field = [["~" for _ in range(10)] for _ in range(10)]
+        self.field = self._create_field(ships)
 
-    def fire(self, location: tuple):
-        # This function should check whether the location
-        # is a key in the `self.field`
-        # If it is, then it should check if this cell is the last alive
-        # in the ship or not.
-        pass
+    def _create_field(
+            self,
+            ships: list[tuple[tuple[int, int], tuple[int, int]]]
+    ) -> dict[tuple[tuple[int, int]], Ship]:
+        return {
+            self._get_ship_coordinates(ship): Ship(start=ship[0], end=ship[1])
+            for ship in ships
+        }
+
+    def _get_ship_coordinates(self, ship: tuple) -> tuple[tuple[int, int]]:
+        start = ship[0]
+        end = ship[1]
+
+        return tuple(
+            self._get_deck_coordinates_after_initial_marking(row, column)
+            for row in range(start[0], end[0] + 1)
+            for column in range(start[1], end[1] + 1)
+        )
+
+    def fire(self, location: tuple) -> str:
+        for coordinates, ship in self.field.items():
+            if location in coordinates:
+                ship.fire(row=location[0], column=location[1])
+                self._mark_actual_deck_status_onto_field(
+                    ship=ship, row=location[0], column=location[1]
+                )
+                return "Sunk!" if ship.is_drowned else "Hit!"
+        return "Miss!"
+
+    def print_field(self) -> None:
+        for row in self.printed_field:
+            print(" ".join(row))
+
+    def _get_deck_coordinates_after_initial_marking(
+            self,
+            row: int,
+            column: int
+    ) -> tuple[int, int]:
+        self.printed_field[row][column] = u"\u25A1"
+        return row, column
+
+    def _mark_actual_deck_status_onto_field(
+            self,
+            ship: Ship,
+            row: int,
+            column: int
+    ) -> None:
+        if ship.is_drowned:
+            for deck in ship.decks:
+                self.printed_field[deck.row][deck.column] = "x"
+        else:
+            self.printed_field[row][column] = "*"
