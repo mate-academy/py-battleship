21940e63265d6ae8aa3013faef586a79099dcbb8
DenZatolokin <denzatolokin@gmail.com>
2024-02-14 01:12:24 +0200

    Step 2

--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
-import numpy as np
-
-
 class Deck:
-    def __init__(self, row, column, is_alive=True):
+    def __init__(self, row, column, is_alive=True) -> None:
         self.row = row
         self.column = column
         self.is_alive = is_alive
@@ -10,13 +7,19 @@ class Deck:
 
 class Ship:
     def __init__(self, start: tuple, end: tuple, is_drowned: bool = False) -> None:
-        self.start = start
-        self.end = end
+        self.decks: list = self._get_deck(start, end)
         self.is_drowned = is_drowned
-        self.is_alive = []
 
-    def get_deck(self, row, column):
-        return
+    @staticmethod
+    def _get_deck(start, end) -> list:
+        x1, y1 = start
+        x2, y2 = end
+        if x1 == x2:
+            return [(x1, y) for y in range(
+                min(y1, y2), max(y1, y2) + 1)]
+        elif y1 == y2:
+            return [(x, y1) for x in range(
+                min(x1, x2), max(x1, x2) + 1)]
 
     def fire(self, row, column):
         # Change the `is_alive` status of the deck
@@ -25,17 +28,44 @@ class Ship:
 
 
 class Battleship:
-    def __init__(self, ships: list) -> None:
-        self.ships = ships
-        self.field = [["~"] * 10] * 10
+    field = [["~" for _ in range(10)] for _ in range(10)]
 
+    def __init__(self, ships: list[tuple]) -> None:
+        self.ships = [Ship(start, end) for start, end in ships]
+        self.ships_on_the_field()
 
-    def fire(self, location: tuple):
+    def ships_on_the_field(self) -> None:
+        for ship in self.ships:
+            for deck in ship.decks:
+                x1, y1 = deck
+                Battleship.field[x1][y1] = u"\u25A1"
+                # print(f"{deck}")
+
+    def fire(self: tuple):
         # This function should check whether the location
         # is a key in the `self.field`
         # If it is, then it should check if this cell is the last alive
         # in the ship or not.
         pass
 
-    def print_field(self) -> None:
-        print(np.array(self.field))
+    @staticmethod
+    def print_field() -> None:
+        for row in Battleship.field:
+            print("  ".join(row))
+
+
+battle_ship = Battleship(
+        ships=[
+            ((0, 0), (0, 3)),
+            ((0, 5), (0, 6)),
+            ((0, 8), (0, 9)),
+            ((2, 0), (4, 0)),
+            ((2, 4), (2, 6)),
+            ((2, 8), (2, 9)),
+            ((9, 9), (9, 9)),
+            ((7, 7), (7, 7)),
+            ((7, 9), (7, 9)),
+            ((9, 7), (9, 7)),
+        ]
+    )
+print(battle_ship.print_field())
