7206c3ad8caecdcc7f91e61ac62de77418029994
DenZatolokin <denzatolokin@gmail.com>
2024-02-15 22:46:37 +0200

    Done

--- app/main.py
+++ app/main.py
@@ -1,17 +1,35 @@
 class Deck:
-    def __init__(self, row, column, is_alive=True) -> None:
+    visible_symbol = u"\u25A1  "
+    destroyed_symbol = "x  "
+
+    def __init__(
+            self,
+            row: int,
+            column: int,
+            is_alive: bool = True
+    ) -> None:
         self.row = row
         self.column = column
         self.is_alive = is_alive
 
+    def __str__(self) -> str:
+        return self.visible_symbol if self.is_alive else self.destroyed_symbol
+
 
 class Ship:
-    def __init__(self, start: tuple, end: tuple, is_drowned: bool = False) -> None:
-        self.decks: list = self._get_deck(start, end)
+    def __init__(
+            self,
+            start: tuple,
+            end: tuple,
+            is_drowned: bool = False
+    ) -> None:
+        self.decks: list[Deck] = [
+            Deck(row, column) for row, column in self._get_deck(start, end)
+        ]
         self.is_drowned = is_drowned
 
     @staticmethod
-    def _get_deck(start, end) -> list:
+    def _get_deck(start: tuple, end: tuple) -> list:
         x1, y1 = start
         x2, y2 = end
         if x1 == x2:
@@ -21,10 +39,14 @@ class Ship:
             return [(x, y1) for x in range(
                 min(x1, x2), max(x1, x2) + 1)]
 
-    def fire(self, row, column):
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
-        pass
+    def fire(self, row: int, column: int) -> bool:
+        for deck in self.decks:
+            if deck.row == row and deck.column == column:
+                deck.is_alive = False
+                if all(not deck.is_alive for deck in self.decks):
+                    self.is_drowned = True
+                return True
+        return False
 
 
 class Battleship:
@@ -38,38 +60,31 @@ class Battleship:
         result = ""
         for row in self.field:
             for item in row:
-                if item == "~":
-                    result += item + "  "
+                if isinstance(item, Deck):
+                    result += str(item)
                 else:
-                    result += u"\u25A1  "
+                    result += item + "  "
             result += "\n"
         return result
 
     def ships_on_the_field(self) -> None:
         for ship in self.ships:
             for deck in ship.decks:
-                x1, y1 = deck
-                self.field[x1][y1] = ship
+                x1, y1 = deck.row, deck.column
+                self.field[x1][y1] = deck
 
-    def fire(self, location: tuple) -> None:
-        if location in self.field:
-
-            ...
-
-
-battle_ship = Battleship(
-        ships=[
-            ((0, 0), (0, 3)),
-            ((0, 5), (0, 6)),
-            ((0, 8), (0, 9)),
-            ((2, 0), (4, 0)),
-            ((2, 4), (2, 6)),
-            ((2, 8), (2, 9)),
-            ((9, 9), (9, 9)),
-            ((7, 7), (7, 7)),
-            ((7, 9), (7, 9)),
-            ((9, 7), (9, 7)),
-        ]
-    )
-battle_ship.fire((0, 4))
-print(battle_ship)
+    def fire(self, location: tuple) -> str:
+        try:
+            x1, y1 = location
+            target = self.field[x1][y1]
+            if isinstance(target, Deck):
+                if target.is_alive:
+                    for ship in self.ships:
+                        if target in ship.decks:
+                            ship.fire(x1, y1)
+                            if ship.is_drowned:
+                                return "Sunk!"
+                    return "Hit!"
+            return "Miss!"
+        except IndexError:
+            return "Invalid coordinates!"
