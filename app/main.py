0c91e6c761aadcae2cb7f27a617aaaf0cb420a8c
Vasyl Kalish <vasyl.documents@yahoo.it>
2024-02-24 18:00:14 +0100

    fix task

--- app/main.py
+++ app/main.py
@@ -17,24 +17,11 @@ class Ship:
         end: tuple,
         is_drowned: bool = False
     ) -> None:
-        self.start = start
-        self.end = end
         self.is_drowned = is_drowned
         self.decks = []
-        st = self.start[0]
-        fix_pos = self.end[1]
-        iter_pos = self.end[0]
-        row = True
-        if self.start[0] == self.end[0]:
-            row = False
-            fix_pos = self.end[0]
-            iter_pos = self.end[1]
-            st = self.start[1]
-        for deck in range(st, iter_pos + 1):
-            if not row:
-                self.decks.append(Deck(fix_pos, deck))
-            else:
-                self.decks.append(Deck(deck, fix_pos))
+        for row in range(start[0], end[0] + 1):
+            for column in range(start[1], end[1] + 1):
+                self.decks.append(Deck(row, column))
 
     def get_deck(
         self,
@@ -52,43 +39,46 @@ class Ship:
     ) -> None:
         deck = self.get_deck(row, column)
         deck.is_alive = False
-        ship_status = [cell.is_alive for cell in self.decks]
-        if not any(ship_status):
-            self.is_drowned = True
+        self.is_drowned = not any([cell.is_alive for cell in self.decks])
 
 
 class Battleship:
     def __init__(self, ships: list) -> None:
-        self.ships = ships
-        self.field = [[None for _ in range(10)] for _ in range(10)]
-        self.ships = [Ship(value[0], value[1]) for value in self.ships]
-        for ship in self.ships:
-            for cell in ship.decks:
-                self.field[cell.row][cell.column] = ship
+        self.field = {}
+        for ship_start, ship_end in ships:
+            ship = Ship(ship_start, ship_end)
+            for deck in ship.decks:
+                self.field[(deck.row, deck.column)] = ship
 
     def fire(self, location: tuple) -> str:
-        field_loc = self.field[location[0]][location[1]]
-        if field_loc is None:
+        try:
+            ship = self.field[location]
+            ship .fire(location[0], location[1])
+            if ship .is_drowned:
+                return "Sunk!"
+            return "Hit!"
+        except KeyError:
             return "Miss!"
-        field_loc.fire(location[0], location[1])
-        if field_loc.is_drowned:
-            return "Sunk!"
-        return "Hit!"
 
     def print_field(self) -> None:
-        for index, row in enumerate(self.field):
-            for pos, column in enumerate(row):
-                if column is None:
-                    self.field[index][pos] = "~"
-                else:
-                    if column.is_drowned:
-                        self.field[index][pos] = "x"
-                    elif column.get_deck(index, pos).is_alive:
-                        self.field[index][pos] = u"\u25A1"
+        new_field = {}
+        for row in range(10):
+            row_field = {}
+            for column in range(10):
+                location = (row, column)
+                try:
+                    if self.field[location].is_drowned:
+                        row_field[location] = "x"
+                    elif self.field[location].get_deck(row, column).is_alive:
+                        row_field[location] = u"\u25A1"
                     else:
-                        self.field[index][pos] = "*"
-        for row in self.field:
-            for column in row:
+                        row_field[location] = "*"
+                except KeyError:
+                    row_field[location] = "~"
+            new_field.update(row_field)
+
+        for row in range(10):
+            for column in range(10):
                 space = 6 * " "
-                print(f"{column}{space}", end="")
+                print(f"{new_field[(row, column)]}{space}", end="")
             print("\n")
