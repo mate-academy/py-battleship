2f6c0239d5874bdac660ae53188b143e09dbc642
Alex Grytsai <a.grytsai1987@gmail.com>
2024-02-24 08:02:08 +0200

    Fineshed Ship.
    Need finish _validate_field and fire

--- app/main.py
+++ app/main.py
@@ -1,27 +1,40 @@
 class Deck:
     def __init__(self, row, column, is_alive=True):
-        pass
+        self.row = row
+        self.column = column
+        self.is_alive = is_alive
 
 
 class Ship:
-    decks = []
 
     def __init__(self, start: tuple, end: tuple, is_drowned=False) -> None:
-        # Create decks and save them to a list `self.decks`
-        self.decks.append(Deck(start, end, is_drowned))
+        self.start = start
+        self.end = end
+        self.is_drowned = is_drowned
+        self.decks = []
+        if start == end:
+            self.decks = Deck(start, end)
+        if start[0] == end[0]:
+            self.decks = [Deck(start[0], end[0] + i)
+                          for i in range(abs(start[1] - end[1]) + 1)]
+        if start[1] == end[1]:
+            self.decks = [Deck(start[1] + i, end[1])
+                          for i in range(abs(start[0] - end[0]) + 1)]
 
-    def get_deck(self, row, column):
-        # Find the corresponding deck in the list
-        pass
+    def get_deck(self, row: int, column: int) -> Deck | None:
+        for deck in self.decks:
+            if deck.row == row and deck.column == column and deck.is_alive:
+                return deck
 
     def fire(self, row, column):
-        # Change the `is_alive` status of the deck
-        # And update the `is_drowned` value if it's needed
-        pass
+        attacked_deck = self.get_deck(row, column)
+        if attacked_deck is not None:
+            attacked_deck.is_alive = False
+            if not all(self.decks):
+                self.is_drowned = True
 
 
 class Battleship:
-    field = {}
 
     @staticmethod
     def _validate_field(ships: list[tuple]) -> bool:
@@ -50,22 +63,19 @@ class Battleship:
             raise ValueError("there should be 2 three-deck ships")
         if fleet["four_deck"] != 1:
             raise ValueError("there should be 1 four-deck ship")
-        #TODO: need add check locate ships
+        # TODO: need add check locate ships
 
-
-    def __init__(self, ships):
-        # Create a dict `self.field`.
-        # Its keys are tuples - the coordinates of the non-empty cells,
-        # A value for each cell is a reference to the ship
-        # which is located in it
+    def __init__(self, ships: list[tuple[tuple, tuple]]) -> None:
+        self.field = {}
         self._validate_field(ships)
-        for ship in ships:
-            start, end = ship
-            self.field[ship] = Ship(start, end)
+        for coordinates_ship in ships:
+            ship = Ship(coordinates_ship[0], coordinates_ship[1])
+            for deck in ship.decks:
+                self.field[deck.row, deck.column] = ship
 
-    def __str__(self):
-        return '\n'.join([' '.join(['~' for _ in range(10)])
-                          for _ in range(10)])
+    # def __str__(self):
+    #     return '\n'.join([' '.join(['~' for _ in range(10)])
+    #                       for _ in range(10)])
 
     def fire(self, location: tuple):
         # This function should check whether the location
@@ -89,4 +99,4 @@ battle_ship = Battleship(
         ((9, 7), (9, 7)),
     ]
 )
-print(battle_ship)
+print(battle_ship.field)
