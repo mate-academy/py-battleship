8d20c7247f03a131f3213d39721e9f4052c7c217
Yuliia Dudych <juliadudych@gmail.com>
2024-03-10 22:37:23 +0200

    Add '_validate_field' method and related 'GameRules' class

--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
+class GameRules:
+    def __init__(self) -> None:
+        self.ship_num = 10
+        self.ships_amount = {1: 4, 2: 3, 3: 2, 4: 1}
+
+
 class Deck:
     def __init__(self,
                  row: int,
@@ -35,6 +41,7 @@ class Battleship:
         self.field = {
             deck.location: ship for ship in self.ships for deck in ship.decks
         }
+        self._validate_field()
 
     def fire(self, location: tuple) -> str:
         if location in self.field:
@@ -43,3 +50,32 @@ class Battleship:
             return "Sunk!" if ship.is_drowned else "Hit!"
         else:
             return "Miss!"
+
+    def _validate_field(self) -> None:
+        rules = GameRules()
+
+        if len(self.ships) != rules.ship_num:
+            raise Exception(f"Number of ships should be {rules.ship_num}")
+
+        ship_sizes = [len(ship.decks) for ship in self.ships]
+        for size, num in rules.ships_amount.items():
+            if ship_sizes.count(size) != num:
+                raise Exception(f"Number of {size}-deck ships should be {num}")
+
+        neighboring_cells = [
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1), (0, 0), (0, 1),
+            (1, -1), (1, 0), (1, 1)
+        ]
+
+        for ship in self.ships:
+            empty = []
+            for deck in ship.decks:
+                cell = deck.location
+                for point in neighboring_cells:
+                    empty.append((cell[0] + point[0], cell[1] + point[1]))
+            for item in empty:
+                if item in self.field and self.field[item] != ship:
+                    raise Exception(
+                        "Ships cannot be located in neighboring cells"
+                    )
